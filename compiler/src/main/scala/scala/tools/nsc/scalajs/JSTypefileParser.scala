06652877a87728a2b41bacc1884cb94ba34b6b18
Sébastien Doeraene <sjrdoeraene@gmail.com>
2013-03-13 11:15:07 +0100

    Added support for separate compilation (load .jstype files).
    The JSPlatform provides a specialized classpath context that
    is able to load symbol information from .jstype files, in
    addition to .class files.

---
+++ compiler/src/main/scala/scala/tools/nsc/scalajs/JSTypefileParser.scala
@@ -0,0 +1,57 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2013 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+
+package scala.tools.nsc
+package scalajs
+
+import java.io.IOException
+import io.AbstractFile
+
+/** This abstract class implements a jstype file parser.
+ *
+ *  @author Sébastien Doeraene
+ *  @version 1.0
+ */
+abstract class JSTypefileParser {
+  val global: JSGlobal
+
+  import global._
+
+  protected var busy: Option[Symbol] = None // lock to detect recursive reads
+
+  protected var srcfile0: Option[AbstractFile] = None
+  def srcfile = srcfile0
+
+  private object unpickler extends scala.reflect.internal.pickling.UnPickler {
+    val global: JSTypefileParser.this.global.type = JSTypefileParser.this.global
+  }
+
+  @inline private def pushBusy[T](sym: Symbol)(body: => T): T = {
+    busy match {
+      case Some(`sym`)  => throw new IOException("unsatisfiable cyclic dependency in '%s'".format(sym))
+      case Some(sym1)   => throw new IOException("illegal class file dependency between '%s' and '%s'".format(sym, sym1))
+      case _            => ()
+    }
+
+    busy = Some(sym)
+    try body
+    finally busy = None
+  }
+
+  def parse(file: AbstractFile, root: Symbol): Unit = {
+    debuglog("[class] >> " + root.fullName)
+
+    pushBusy(root) {
+      val fileBytes = file.toByteArray
+      val clazz = if (root.isModule) root.companionClass else root
+      // WARNING! do no use clazz.companionModule to find staticModule.
+      // In a situation where root can be defined, but its companionClass not,
+      // this would give incorrect results (see SI-5031 in separate compilation scenario)
+      val staticModule = if (root.isModule) root else root.companionModule
+
+      unpickler.unpickle(fileBytes, 0, clazz, staticModule, file.name)
+    }
+  }
+}
