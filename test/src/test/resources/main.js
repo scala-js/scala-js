00debad3bd6b05e2381956e831373d4cb01500d0
Sébastien Doeraene <sjrdoeraene@gmail.com>
2013-11-27 16:59:03 +0100

    Throw an exception when at least one test failed.
    This causes an invocation to `sbt scalajs-test/test` to
    actually return an exit code of 1 if a test fails.
    This in turn will cause the Travis build to fail if
    there is at least one test failing.

--- test/src/test/resources/main.js
+++ test/src/test/resources/main.js
@@ -29,3 +29,6 @@ var jasmineEnv = jasmine.getEnv();
 jasmineEnv.addReporter(new jasmine.RhinoReporter());
 jasmineEnv.updateInterval = 0;
 jasmineEnv.execute();
+
+if (jasmineEnv.currentRunner().results().failedCount > 0)
+  throw new Error("Some tests failed")
