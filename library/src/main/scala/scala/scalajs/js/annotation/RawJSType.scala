9b7992a5ff8452734b79c020ce995c77fd1b36fd
Tobias Schlatter <tobias@meisch.ch>
2013-12-12 17:07:23 +0100

    add js-interop phase to compiler and have it tag raw js classes

---
+++ library/src/main/scala/scala/scalajs/js/annotation/RawJSType.scala
@@ -0,0 +1,17 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-2013, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+
+
+package scala.scalajs.js.annotation
+
+/** Marks the targeted class as raw javascript type
+ *  
+ *  Do not use in code. Is automatically added by the compiler  
+ */
+class RawJSType extends scala.annotation.StaticAnnotation
