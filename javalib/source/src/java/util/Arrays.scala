76fe9b1aa5a10959df4b357b9c0720a797d34cf4
Piotr Kolaczkowski <pkolaczk@datastax.com>
2013-12-31 16:23:53 +0100

    #93: Add complete set of fill methods for java.util.Arrays.

--- javalib/source/src/java/util/Arrays.scala
+++ javalib/source/src/java/util/Arrays.scala
@@ -12,6 +12,70 @@ object Arrays {
     jsArray.sort(compareFn _)
   }
 
+  def fill(a: Array[Boolean], value: Boolean): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Boolean], fromIndex: Int, toIndex: Int, value: Boolean): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Byte], value: Byte): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Byte], fromIndex: Int, toIndex: Int, value: Byte): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Char], value: Char): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Char], fromIndex: Int, toIndex: Int, value: Char): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Short], value: Short): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Short], fromIndex: Int, toIndex: Int, value: Short): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
   def fill(a: Array[Int], value: Int): Unit = {
     var i = 0
     while (i < a.length) {
@@ -19,4 +83,77 @@ object Arrays {
       i += 1
     }
   }
+
+  def fill(a: Array[Int], fromIndex: Int, toIndex: Int, value: Int): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Long], value: Long): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Long], fromIndex: Int, toIndex: Int, value: Long): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Float], value: Float): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Float], fromIndex: Int, toIndex: Int, value: Float): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Double], value: Double): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[Double], fromIndex: Int, toIndex: Int, value: Double): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[AnyRef], value: AnyRef): Unit = {
+    var i = 0
+    while (i < a.length) {
+      a(i) = value
+      i += 1
+    }
+  }
+
+  def fill(a: Array[AnyRef], fromIndex: Int, toIndex: Int, value: AnyRef): Unit = {
+    var i = fromIndex
+    while (i < toIndex) {
+      a(i) = value
+      i += 1
+    }
+  }
+
 }
--- test/src/test/scala/scala/scalajs/test/javalib/ArraysTest.scala
+++ test/src/test/scala/scala/scalajs/test/javalib/ArraysTest.scala
@@ -44,6 +44,57 @@ object ArraysTest extends ScalaJSTest {
       expect(scalajs).toEqual(sorted)
     }
 
+    it("should respond to `fill` for Boolean") {
+      val booleans = new Array[Boolean](6)
+      Arrays.fill(booleans, false)
+      expect(booleans).toEqual(Array(false, false, false, false, false, false))
+
+      Arrays.fill(booleans, true)
+      expect(booleans).toEqual(Array(true, true, true, true, true, true))
+    }
+
+    it("should respond to `fill` with start and end index for Boolean") {
+      val booleans = new Array[Boolean](6)
+      Arrays.fill(booleans, 1, 4, true)
+      expect(booleans).toEqual(Array(false, true, true, true, false, false))
+    }
+    
+    it("should respond to `fill` for Byte") {
+      val bytes = new Array[Byte](6)
+      Arrays.fill(bytes, 42.toByte)
+      expect(bytes).toEqual(Array[Byte](42, 42, 42, 42, 42, 42))
+
+      Arrays.fill(bytes, -1.toByte)
+      expect(bytes).toEqual(Array[Byte](-1, -1, -1, -1, -1, -1))
+    }
+
+    it("should respond to `fill` with start and end index for Byte") {
+      val bytes = new Array[Byte](6)
+      Arrays.fill(bytes, 1, 4, 42.toByte)
+      expect(bytes).toEqual(Array[Byte](0, 42, 42, 42, 0, 0))
+
+      Arrays.fill(bytes, 2, 5, -1.toByte)
+      expect(bytes).toEqual(Array[Byte](0, 42, -1, -1, -1, 0))
+    }
+
+    it("should respond to `fill` for Short") {
+      val shorts = new Array[Short](6)
+      Arrays.fill(shorts, 42.toShort)
+      expect(shorts).toEqual(Array[Short](42, 42, 42, 42, 42, 42))
+
+      Arrays.fill(shorts, -1.toShort)
+      expect(shorts).toEqual(Array[Short](-1, -1, -1, -1, -1, -1))
+    }
+
+    it("should respond to `fill` with start and end index for Short") {
+      val shorts = new Array[Short](6)
+      Arrays.fill(shorts, 1, 4, 42.toShort)
+      expect(shorts).toEqual(Array[Short](0, 42, 42, 42, 0, 0))
+
+      Arrays.fill(shorts, 2, 5, -1.toShort)
+      expect(shorts).toEqual(Array[Short](0, 42, -1, -1, -1, 0))
+    }
+
     it("should respond to `fill` for Int") {
       val ints = new Array[Int](6)
       Arrays.fill(ints, 42)
@@ -51,6 +102,87 @@ object ArraysTest extends ScalaJSTest {
 
       Arrays.fill(ints, -1)
       expect(ints).toEqual(Array(-1, -1, -1, -1, -1, -1))
+    }
+
+    it("should respond to `fill` with start and end index for Int") {
+      val ints = new Array[Int](6)
+      Arrays.fill(ints, 1, 4, 42)
+      expect(ints).toEqual(Array(0, 42, 42, 42, 0, 0))
+
+      Arrays.fill(ints, 2, 5, -1)
+      expect(ints).toEqual(Array(0, 42, -1, -1, -1, 0))
+    }
+
+    it("should respond to `fill` for Long") {
+      val longs = new Array[Long](6)
+      Arrays.fill(longs, 42L)
+      expect(longs).toEqual(Array(42L, 42L, 42L, 42L, 42L, 42L))
+
+      Arrays.fill(longs, -1L)
+      expect(longs).toEqual(Array(-1L, -1L, -1L, -1L, -1L, -1L))
+    }
+
+    it("should respond to `fill` with start and end index for Long") {
+      val longs = new Array[Long](6)
+      Arrays.fill(longs, 1, 4, 42L)
+      expect(longs).toEqual(Array(0L, 42L, 42L, 42L, 0L, 0L))
+
+      Arrays.fill(longs, 2, 5, -1L)
+      expect(longs).toEqual(Array(0L, 42L, -1L, -1L, -1L, 0L))
+    }
+
+    it("should respond to `fill` for Float") {
+      val floats = new Array[Float](6)
+      Arrays.fill(floats, 42.0f)
+      expect(floats).toEqual(Array(42.0f, 42.0f, 42.0f, 42.0f, 42.0f, 42.0f))
+
+      Arrays.fill(floats, -1.0f)
+      expect(floats).toEqual(Array(-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f))
+    }
+
+    it("should respond to `fill` with start and end index for Float") {
+      val floats = new Array[Float](6)
+      Arrays.fill(floats, 1, 4, 42.0f)
+      expect(floats).toEqual(Array(0.0f, 42.0f, 42.0f, 42.0f, 0.0f, 0.0f))
+
+      Arrays.fill(floats, 2, 5, -1.0f)
+      expect(floats).toEqual(Array(0.0f, 42.0f, -1.0f, -1.0f, -1.0f, 0.0f))
+    }
+
+    it("should respond to `fill` for Double") {
+      val doubles = new Array[Double](6)
+      Arrays.fill(doubles, 42.0)
+      expect(doubles).toEqual(Array(42.0, 42.0, 42.0, 42.0, 42.0, 42.0))
+
+      Arrays.fill(doubles, -1.0f)
+      expect(doubles).toEqual(Array(-1.0, -1.0, -1.0, -1.0, -1.0, -1.0))
+    }
+
+    it("should respond to `fill` with start and end index for Double") {
+      val doubles = new Array[Double](6)
+      Arrays.fill(doubles, 1, 4, 42.0)
+      expect(doubles).toEqual(Array(0.0, 42.0, 42.0, 42.0, 0.0, 0.0))
+
+      Arrays.fill(doubles, 2, 5, -1.0)
+      expect(doubles).toEqual(Array(0.0, 42.0, -1.0, -1.0, -1.0, 0.0))
+    }
+
+    it("should respond to `fill` for AnyRef") {
+      val array = new Array[AnyRef](6)
+      Arrays.fill(array, "a")
+      expect(array).toEqual(Array[AnyRef]("a", "a", "a", "a", "a", "a"))
+
+      Arrays.fill(array, "b")
+      expect(array).toEqual(Array[AnyRef]("b", "b", "b", "b", "b", "b"))
+    }
+
+    it("should respond to `fill` with start and end index for AnyRef") {
+      val bytes = new Array[AnyRef](6)
+      Arrays.fill(bytes, 1, 4, "a")
+      expect(bytes).toEqual(Array[AnyRef](null, "a", "a", "a", null, null))
+
+      Arrays.fill(bytes, 2, 5, "b")
+      expect(bytes).toEqual(Array[AnyRef](null, "a", "b", "b", "b", null))
     }
 
   }
