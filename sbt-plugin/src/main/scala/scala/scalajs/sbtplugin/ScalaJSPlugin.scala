3aec66e7839d5f782ec1fa4221b33c36b2c8807a
Tobias Schlatter <tobias@meisch.ch>
2014-04-24 15:52:58 +0200

    Towards 0.4.4.

--- sbt-plugin/src/main/scala/scala/scalajs/sbtplugin/ScalaJSPlugin.scala
+++ sbt-plugin/src/main/scala/scala/scalajs/sbtplugin/ScalaJSPlugin.scala
@@ -32,7 +32,7 @@ import environment.rhino.{CodeBlock, Utilities}
 import scala.scalajs.sbtplugin.testing.TestFramework
 
 object ScalaJSPlugin extends Plugin {
-  val scalaJSVersion = "0.4.3"
+  val scalaJSVersion = "0.4.4-SNAPSHOT"
   val scalaJSIsSnapshotVersion = scalaJSVersion endsWith "-SNAPSHOT"
   val scalaJSScalaVersion = "2.10.2"
 
