d8f62b348ff54edd70cd11041c2d43afb289e22e
Tobias Schlatter <tobias@meisch.ch>
2014-04-29 11:41:46 +0200

    Fix #554: Disable closure warnings for fractional bitwise operand

---
+++ tools/src/main/scala/scala/scalajs/tools/optimizer/LoggerErrorManager.scala
@@ -0,0 +1,38 @@
+package scala.scalajs.tools.optimizer
+
+import com.google.javascript.jscomp.{ BasicErrorManager, CheckLevel, JSError }
+
+import scala.scalajs.tools.logging.Logger
+
+/** A Google Closure Error Manager that forwards to a tools.logging.Logger */
+class LoggerErrorManager(private val log: Logger) extends BasicErrorManager {
+
+  /** Ugly hack to disable FRACTIONAL_BITWISE_OPERAND warnings. Since its
+    * DiagnosticType (PeepholeFoldConstants.FRACTIONAL_BITWISE_OPERAND) is
+    * package private.
+    */
+  override def report(level: CheckLevel, error: JSError): Unit = {
+    if (error.getType.key == "JSC_FRACTIONAL_BITWISE_OPERAND")
+      super.report(CheckLevel.OFF, error)
+    else
+      super.report(level, error)
+  }
+
+  def println(level: CheckLevel, error: JSError): Unit = level match {
+    case CheckLevel.WARNING => log.warn (s"Closure: ${error}")
+    case CheckLevel.ERROR   => log.error(s"Closure: ${error}")
+    case CheckLevel.OFF     =>
+  }
+
+  protected def printSummary(): Unit = {
+    val msg = s"Closure: ${getErrorCount} error(s), ${getWarningCount} warning(s)"
+
+    if (getErrorCount > 0)
+      log.error(msg)
+    else if (getWarningCount > 0)
+      log.warn(msg)
+    else
+      log.info(msg)
+  }
+
+}
