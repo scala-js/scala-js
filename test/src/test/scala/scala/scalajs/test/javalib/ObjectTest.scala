4e085d3a8a8a08b1f406d4138d71b9d3e2ff9abb
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-05-10 20:28:12 +0200

    Fix #632: Everything but null should be a java.lang.Object.

--- test/src/test/scala/scala/scalajs/test/javalib/ObjectTest.scala
+++ test/src/test/scala/scala/scalajs/test/javalib/ObjectTest.scala
@@ -10,6 +10,8 @@ package javalib
 
 import scala.scalajs.test.JasmineTest
 
+import scala.scalajs.js
+
 object ObjectTest extends JasmineTest {
 
   describe("java.lang.Object") {
@@ -22,6 +24,49 @@ object ObjectTest extends JasmineTest {
 
       expect(l.contains(xy12)).toBeTruthy
       expect(l.exists(_ == xy12)).toBeTruthy // the workaround
+    }
+
+    it("everything but null should be an Object") {
+      expect((()             : Any).isInstanceOf[Object]).toBeTruthy
+      expect((true           : Any).isInstanceOf[Object]).toBeTruthy
+      expect(('a'            : Any).isInstanceOf[Object]).toBeTruthy
+      expect((1.toByte       : Any).isInstanceOf[Object]).toBeTruthy
+      expect((658.toShort    : Any).isInstanceOf[Object]).toBeTruthy
+      expect((60000          : Any).isInstanceOf[Object]).toBeTruthy
+      expect((12345678910112L: Any).isInstanceOf[Object]).toBeTruthy
+      expect((6.5f           : Any).isInstanceOf[Object]).toBeTruthy
+      expect((12.4           : Any).isInstanceOf[Object]).toBeTruthy
+      expect((new Object     : Any).isInstanceOf[Object]).toBeTruthy
+      expect(("hello"        : Any).isInstanceOf[Object]).toBeTruthy
+      expect((List(1)        : Any).isInstanceOf[Object]).toBeTruthy
+      expect((Array(1)       : Any).isInstanceOf[Object]).toBeTruthy
+      expect((Array(Nil)     : Any).isInstanceOf[Object]).toBeTruthy
+      expect((new js.Object  : Any).isInstanceOf[Object]).toBeTruthy
+      expect((js.Array(5)    : Any).isInstanceOf[Object]).toBeTruthy
+    }
+
+    it("null should not be an Object") {
+      expect((null: Any).isInstanceOf[Object]).toBeFalsy
+    }
+
+    it("everything should cast to Object successfully, including null") {
+      expect(() => (()             : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (true           : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => ('a'            : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (1.toByte       : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (658.toShort    : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (60000          : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (12345678910112L: Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (6.5f           : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (12.4           : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (new Object     : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => ("hello"        : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (List(1)        : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (Array(1)       : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (Array(Nil)     : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (new js.Object  : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (js.Array(5)    : Any).asInstanceOf[Object]).not.toThrow
+      expect(() => (null           : Any).asInstanceOf[Object]).not.toThrow
     }
 
   }
