afac47d2f8d981fa5eead7b3e95f7ba7039040ab
Tobias Schlatter <tobias@meisch.ch>
2014-05-22 18:36:19 +0200

    Continuing 0.5.0.

--- ir/src/main/scala/scala/scalajs/ir/ScalaJSVersions.scala
+++ ir/src/main/scala/scala/scalajs/ir/ScalaJSVersions.scala
@@ -3,7 +3,7 @@ package scala.scalajs.ir
 object ScalaJSVersions {
 
   /** the Scala.js version of this build */
-  final val current = "0.5.0-M3"
+  final val current = "0.5.0-SNAPSHOT"
 
   /** true iff the Scala.js version of this build is a snapshot version. */
   final val currentIsSnapshot = current endsWith "-SNAPSHOT"
