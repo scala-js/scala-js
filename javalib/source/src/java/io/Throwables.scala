7d05c680dbc729c9ff52209f3fa4f842bc179748
Tobias Schlatter <tobias@meisch.ch>
2014-07-10 23:26:38 +0200

    Add javalibEx with DataInputStream

--- javalib/source/src/java/io/Throwables.scala
+++ javalib/source/src/java/io/Throwables.scala
@@ -5,3 +5,11 @@ class IOException(s: String, e: Throwable) extends Exception(s) {
   def this(s: String) = this(s, null)
   def this() = this(null, null)
 }
+
+class EOFException(s: String) extends IOException(s) {
+  def this() = this(null)
+}
+
+class UTFDataFormatException(s: String) extends IOException(s) {
+  def this() = this(null)
+}
