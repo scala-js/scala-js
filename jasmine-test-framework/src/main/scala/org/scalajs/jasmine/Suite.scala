f861814fa660543467ba4facfed7bb5c31e38244
Xavier Cho <fender@eyeq.co.kr>
2014-08-07 00:29:29 +0900

    Change type of 'id' property in Spec/Suite to Int

--- jasmine-test-framework/src/main/scala/org/scalajs/jasmine/Suite.scala
+++ jasmine-test-framework/src/main/scala/org/scalajs/jasmine/Suite.scala
@@ -3,7 +3,7 @@ package org.scalajs.jasmine
 import scala.scalajs.js
 
 trait Suite extends js.Object {
-  val id: String = ???
+  val id: Int = ???
   val description: String = ???
   def results(): SuiteResults = ???
   def specs(): js.Array[Spec] = ???
