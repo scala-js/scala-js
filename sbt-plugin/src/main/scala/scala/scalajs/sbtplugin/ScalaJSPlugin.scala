22c0f515190e19802a4f9da1895f437fd51b7a35
Tobias Schlatter <tobias@meisch.ch>
2014-09-05 22:42:12 +0200

    Fix #1001: Add setting to disable parallel optimization

--- sbt-plugin/src/main/scala/scala/scalajs/sbtplugin/ScalaJSPlugin.scala
+++ sbt-plugin/src/main/scala/scala/scalajs/sbtplugin/ScalaJSPlugin.scala
@@ -104,6 +104,9 @@ object ScalaJSPlugin extends Plugin with impl.DependencyBuilders {
     val inliningMode = SettingKey[InliningMode]("inliningMode",
         "Mode of the inliner: Incremental (default), Batch, Off", CSetting)
 
+    val parallelFastOptJS = SettingKey[Boolean]("parallelFastOptJS",
+        "Whether to use the parallelized Scala.js optimizer", CSetting)
+
     val directFullOptJS = SettingKey[Boolean]("directFullOptJS",
         "Whether fullOptJS should directly use IR produced by fastOptJS", DSetting)
 
