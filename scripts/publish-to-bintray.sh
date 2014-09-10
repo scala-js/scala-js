e0368f23633b66f72aee26aabbb43b467e315e33
Tobias Schlatter <tobias@meisch.ch>
2014-09-10 13:12:14 +0200

    Fix #1006: Publish a JVM library with Scala.js annotations

--- scripts/publish-to-bintray.sh
+++ scripts/publish-to-bintray.sh
@@ -12,7 +12,7 @@ FULL_VERSIONS="2.10.2 2.10.3 2.10.4 2.11.0 2.11.1 2.11.2"
 BIN_VERSIONS="2.10.4 2.11.2"
 SBT_VERSION="2.10.4"
 
-LIBS="library javalibEx jasmineTestFramework tools toolsJS testBridge"
+LIBS="library javalibEx jasmineTestFramework tools toolsJS testBridge stubs"
 
 # Publish compiler
 for v in $FULL_VERSIONS; do
