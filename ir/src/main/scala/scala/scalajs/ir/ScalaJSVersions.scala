4716fcb90e90e8c5b21295c18cb45bf50dbb2c86
Tobias Schlatter <tobias@meisch.ch>
2014-09-19 09:30:05 +0200

    Towards 0.6.0.

--- ir/src/main/scala/scala/scalajs/ir/ScalaJSVersions.scala
+++ ir/src/main/scala/scala/scalajs/ir/ScalaJSVersions.scala
@@ -3,7 +3,7 @@ package scala.scalajs.ir
 object ScalaJSVersions {
 
   /** the Scala.js version of this build */
-  final val current = "0.5.5"
+  final val current = "0.6.0-SNAPSHOT"
 
   /** true iff the Scala.js version of this build is a snapshot version. */
   final val currentIsSnapshot = current endsWith "-SNAPSHOT"
@@ -14,11 +14,10 @@ object ScalaJSVersions {
    *  - a prior release version (i.e. "0.5.0", *not* "0.5.0-SNAPSHOT")
    *  - `current`
    */
-  final val binaryEmitted = "0.5.5"
+  final val binaryEmitted = current
 
   /** Versions whose binary files we can support (used by deserializer) */
-  val binarySupported: Set[String] =
-    Set("0.5.0", "0.5.2", "0.5.3", "0.5.4", "0.5.5", binaryEmitted)
+  val binarySupported: Set[String] = Set(binaryEmitted)
 
   // Just to be extra safe
   assert(binarySupported contains binaryEmitted)
