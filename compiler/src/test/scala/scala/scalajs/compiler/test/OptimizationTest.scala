5c795071cd444422265481ac652507b38e7a0ccd
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-09-22 14:27:43 +0200

    Fix #1091: Improve genPrimitiveJSRepeatedParam for arrays of primitive types.

--- compiler/src/test/scala/scala/scalajs/compiler/test/OptimizationTest.scala
+++ compiler/src/test/scala/scala/scalajs/compiler/test/OptimizationTest.scala
@@ -36,6 +36,40 @@ class OptimizationTest extends JSASTTest {
       case js.New(jstpe.ClassType("sjsr_AnonFunction1"), _, _) =>
     }
 
+    /* Make sure js.Array(...) is optimized away completely for several kinds
+     * of data types.
+     */
+    """
+    import scala.scalajs.js
+
+    class VC(val x: Int) extends AnyVal
+
+    class A {
+      val a = js.Array(5, 7, 9, -3)
+      val b = js.Array("hello", "world")
+      val c = js.Array('a', 'b')
+      val d = js.Array(Nil)
+      val e = js.Array(new VC(151189))
+    }
+    """.
+    hasNot("any of the wrapArray methods") {
+      case js.Apply(_, js.Ident(name, _), _)
+          if name.startsWith("wrap") && name.endsWith("__scm_WrappedArray") =>
+    }
+
+    // Make sure our wrapper matcher has the right name
+    """
+    import scala.scalajs.js
+
+    class A {
+      val a = Array(5, 7, 9, -3)
+    }
+    """.
+    has("one of the wrapArray methods") {
+      case js.Apply(_, js.Ident(name, _), _)
+          if name.startsWith("wrap") && name.endsWith("__scm_WrappedArray") =>
+    }
+
   }
 
 }
