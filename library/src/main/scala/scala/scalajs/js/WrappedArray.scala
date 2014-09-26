129137f55ffa093fc78eb0329711aa64b27a69c3
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-09-26 14:58:25 +0200

    Fix #1115: Enrich js.WrappedArray with Buffer and BufferLike.
    This basically turns it into a full-blown Scala collection,
    whose implementation happens to use a js.Array.
    
    We also mark it @inline, because it gives the BufferLike
    interface to js.Array, which js.ArrayOps cannot provide due to
    the type parameters of BufferLike being too restrictive.

--- library/src/main/scala/scala/scalajs/js/WrappedArray.scala
+++ library/src/main/scala/scala/scalajs/js/WrappedArray.scala
@@ -8,29 +8,34 @@
 
 package scala.scalajs.js
 
+import scala.language.implicitConversions
+
 import scala.collection.mutable
 import mutable.Builder
 
-import scala.collection.generic.CanBuildFrom
+import scala.collection.generic.{CanBuildFrom, GenericCompanion, SeqFactory}
 
 /** Equivalent of scm.WrappedArray for js.Array */
+@inline
 final class WrappedArray[A](val array: Array[A])
     extends mutable.AbstractSeq[A]
+       with scala.collection.generic.GenericTraversableTemplate[A, WrappedArray]
        with mutable.IndexedSeq[A]
+       with mutable.Buffer[A]
+       with mutable.BufferLike[A, WrappedArray[A]]
        with mutable.ArrayLike[A, WrappedArray[A]]
        with Builder[A, WrappedArray[A]] {
 
   /** Creates a new empty [[WrappedArray]]. */
   def this() = this(Array())
 
+  override def companion: GenericCompanion[WrappedArray] = WrappedArray
+
   // IndexedSeq interface
 
-  def update(index: Int, elem: A): Unit = array(index) = elem
-  def apply(index: Int): A = array(index)
-  def length: Int = array.length
-
-  override protected[this] def newBuilder: Builder[A, WrappedArray[A]] =
-    new WrappedArray[A]
+  @inline def update(index: Int, elem: A): Unit = array(index) = elem
+  @inline def apply(index: Int): A = array(index)
+  @inline def length: Int = array.length
 
   // Builder interface
 
@@ -44,19 +49,45 @@ final class WrappedArray[A](val array: Array[A])
 
   @inline def result(): WrappedArray[A] = this
 
+  // Rest of BufferLike interface
+
+  @inline def +=:(elem: A): this.type = {
+    array.unshift(elem)
+    this
+  }
+
+  @inline override def ++=:(xs: TraversableOnce[A]): this.type = {
+    array.unshift(xs.toSeq: _*)
+    this
+  }
+
+  @inline def insertAll(n: Int,
+      elems: scala.collection.Traversable[A]): Unit = {
+    array.splice(n, 0, elems.toSeq: _*)
+  }
+
+  @inline def remove(n: Int): A =
+    array.splice(n, 1)(0)
+
+  @inline override def remove(n: Int, count: Int): Unit =
+    array.splice(n, count)
+
+  @inline override def stringPrefix: String = "WrappedArray"
+
 }
 
-object WrappedArray {
+/** $factoryInfo
+ *  @define coll wrapped array
+ *  @define Coll `WrappedArray`
+ */
+object WrappedArray extends SeqFactory[WrappedArray] {
+  /** $genericCanBuildFromInfo */
+  implicit def canBuildFrom[A]: CanBuildFrom[Coll, A, WrappedArray[A]] =
+    ReusableCBF.asInstanceOf[GenericCanBuildFrom[A]]
 
-  def empty[A]: WrappedArray[A] = new WrappedArray[A]
+  def newBuilder[A]: Builder[A, WrappedArray[A]] = new WrappedArray[A]
 
-  implicit def canBuildFrom[A]: CanBuildFrom[WrappedArray[_], A, WrappedArray[A]] = {
-    new CanBuildFrom[WrappedArray[_], A, WrappedArray[A]] {
-      def apply(from: WrappedArray[_]): Builder[A, WrappedArray[A]] =
-        new WrappedArray[A]
-      def apply: Builder[A, WrappedArray[A]] =
-        new WrappedArray[A]
-    }
-  }
+  implicit def toJSArray[A](wrappedArray: WrappedArray[A]): Array[A] =
+    wrappedArray.array
 
 }
