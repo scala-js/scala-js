2489ba3bcf16a496a5797731ab0e692da0fc6547
Tobias Schlatter <tobias@meisch.ch>
2014-10-22 15:14:18 +0200

    Backport Fix #1169: Make IR printer print cases of match tree nicely
    It now looks like this:
    
        match (x1) {
          case 1 | 2 | 3:
            mod:s_Predef$.println__O__V("low");
          case 4:
            mod:s_Predef$.println__O__V("four");
          default:
            mod:s_Predef$.println__O__V("other");
        }
    
    (cherry picked from commit 0d6504b872d191e600ebf638e5fad0fae6db23ce)
    
    Conflicts:
    
    	ir/src/main/scala/scala/scalajs/ir/Printers.scala

--- ir/src/main/scala/scala/scalajs/ir/Printers.scala
+++ ir/src/main/scala/scala/scalajs/ir/Printers.scala
@@ -267,9 +267,9 @@ object Printers {
         case Match(selector, cases, default) =>
           print("match (", selector, ") ")
           print("{"); indent
-          for ((value, body) <- cases) {
+          for ((values, body) <- cases) {
             println()
-            print("case ", value, ":"); indent; println()
+            printRow(values, "case ", " | ", ":"); indent; println()
             printTree(body, isStat)
             print(";")
             undent
