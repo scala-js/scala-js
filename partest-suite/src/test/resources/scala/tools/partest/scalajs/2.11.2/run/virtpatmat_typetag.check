scala-js/scala-js

commit efe48f93214f376d99e6409fe94acba0af45d620

Introduce Scala.js specific checkfile overrides

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.0/run/t6318_primitives.check
@@ -1,0 +1,54 @@
+Checking if byte matches byte
+Some(1)
+Checking if byte matches short
+None
+Checking if class java.lang.Integer matches byte
+Some(1)
+Checking if short matches short
+Some(1)
+Checking if short matches char
+None
+Checking if class java.lang.Short matches short
+Some(1)
+Checking if char matches char
+Some()
+Checking if char matches int
+None
+Checking if class java.lang.Character matches char
+Some()
+Checking if int matches int
+Some(1)
+Checking if int matches long
+None
+Checking if class java.lang.Integer matches int
+Some(1)
+Checking if long matches long
+Some(1)
+Checking if long matches float
+None
+Checking if class java.lang.Long matches long
+Some(1)
+Checking if float matches float
+Some(1)
+Checking if float matches double
+None
+Checking if class java.lang.Float matches float
+Some(1)
+Checking if double matches double
+Some(1)
+Checking if double matches boolean
+None
+Checking if class java.lang.Double matches double
+Some(1)
+Checking if boolean matches boolean
+Some(true)
+Checking if boolean matches void
+None
+Checking if class java.lang.Boolean matches boolean
+Some(true)
+Checking if void matches void
+Some(undefined)
+Checking if void matches byte
+None
+Checking if class scala.runtime.BoxedUnit matches void
+Some(undefined)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.1/run/t6318_primitives.check
@@ -1,0 +1,54 @@
+Checking if byte matches byte
+Some(1)
+Checking if byte matches short
+None
+Checking if class java.lang.Integer matches byte
+Some(1)
+Checking if short matches short
+Some(1)
+Checking if short matches char
+None
+Checking if class java.lang.Short matches short
+Some(1)
+Checking if char matches char
+Some()
+Checking if char matches int
+None
+Checking if class java.lang.Character matches char
+Some()
+Checking if int matches int
+Some(1)
+Checking if int matches long
+None
+Checking if class java.lang.Integer matches int
+Some(1)
+Checking if long matches long
+Some(1)
+Checking if long matches float
+None
+Checking if class java.lang.Long matches long
+Some(1)
+Checking if float matches float
+Some(1)
+Checking if float matches double
+None
+Checking if class java.lang.Float matches float
+Some(1)
+Checking if double matches double
+Some(1)
+Checking if double matches boolean
+None
+Checking if class java.lang.Double matches double
+Some(1)
+Checking if boolean matches boolean
+Some(true)
+Checking if boolean matches void
+None
+Checking if class java.lang.Boolean matches boolean
+Some(true)
+Checking if void matches void
+Some(undefined)
+Checking if void matches byte
+None
+Checking if class scala.runtime.BoxedUnit matches void
+Some(undefined)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.2/run/t6318_primitives.check
@@ -1,0 +1,54 @@
+Checking if byte matches byte
+Some(1)
+Checking if byte matches short
+None
+Checking if class java.lang.Integer matches byte
+Some(1)
+Checking if short matches short
+Some(1)
+Checking if short matches char
+None
+Checking if class java.lang.Short matches short
+Some(1)
+Checking if char matches char
+Some()
+Checking if char matches int
+None
+Checking if class java.lang.Character matches char
+Some()
+Checking if int matches int
+Some(1)
+Checking if int matches long
+None
+Checking if class java.lang.Integer matches int
+Some(1)
+Checking if long matches long
+Some(1)
+Checking if long matches float
+None
+Checking if class java.lang.Long matches long
+Some(1)
+Checking if float matches float
+Some(1)
+Checking if float matches double
+None
+Checking if class java.lang.Float matches float
+Some(1)
+Checking if double matches double
+Some(1)
+Checking if double matches boolean
+None
+Checking if class java.lang.Double matches double
+Some(1)
+Checking if boolean matches boolean
+Some(true)
+Checking if boolean matches void
+None
+Checking if class java.lang.Boolean matches boolean
+Some(true)
+Checking if void matches void
+Some(undefined)
+Checking if void matches byte
+None
+Checking if class scala.runtime.BoxedUnit matches void
+Some(undefined)

commit eed715e2b584f8ace2ce36318a647fe2042785cc

Fix #1247: Adapt getClass for primitives to Scala.js subtyping
Note that the checkfile override for run/t6318_primitives.check for
Scala 2.11.0 and 2.11.1 has been wrongly introduced in
efe48f93214f376d99e6409fe94acba0af45d620. This commit both fixes the
mistake and adapts them for the change.

tools/scalajsenv.js
@@ -149,10 +149,20 @@ ScalaJS.objectGetClass = function(instance) {
     case "string":
       return ScalaJS.d.T.getClassOf();
     case "number":
-      if (ScalaJS.isInt(instance))
-        return ScalaJS.d.jl_Integer.getClassOf();
-      else
-        return ScalaJS.d.jl_Double.getClassOf();
+      var v = instance | 0;
+      if (v === instance) { // is the value integral?
+        if (ScalaJS.isByte(v))
+          return ScalaJS.d.jl_Byte.getClassOf();
+        else if (ScalaJS.isShort(v))
+          return ScalaJS.d.jl_Short.getClassOf();
+        else
+          return ScalaJS.d.jl_Integer.getClassOf();
+      } else {
+        if (ScalaJS.isFloat(instance))
+          return ScalaJS.d.jl_Float.getClassOf();
+        else
+          return ScalaJS.d.jl_Double.getClassOf();
+      }
     case "boolean":
       return ScalaJS.d.jl_Boolean.getClassOf();
     case "undefined":

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.0/run/t5568.check
@@ -1,0 +1,9 @@
+void
+int
+class scala.runtime.BoxedUnit
+class scala.runtime.BoxedUnit
+class java.lang.Byte
+class java.lang.Byte
+5
+5
+5

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.0/run/t5629b.check
@@ -1,0 +1,10 @@
+=== pf(1):
+MySmartPF.apply entered...
+newPF.applyOrElse entered...
+default
+scala.MatchError: 1 (of class java.lang.Byte)
+=== pf(42):
+MySmartPF.apply entered...
+newPF.applyOrElse entered...
+ok
+=== done

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.0/run/t6318_primitives.check
@@ -1,54 +1,36 @@
-Checking if byte matches byte
+true
 Some(1)
-Checking if byte matches short
+false
 None
-Checking if class java.lang.Integer matches byte
+true
 Some(1)
-Checking if short matches short
-Some(1)
-Checking if short matches char
+false
 None
-Checking if class java.lang.Short matches short
-Some(1)
-Checking if char matches char
+true
 Some()
-Checking if char matches int
+false
 None
-Checking if class java.lang.Character matches char
-Some()
-Checking if int matches int
+true
 Some(1)
-Checking if int matches long
+false
 None
-Checking if class java.lang.Integer matches int
+true
 Some(1)
-Checking if long matches long
-Some(1)
-Checking if long matches float
+false
 None
-Checking if class java.lang.Long matches long
+true
 Some(1)
-Checking if float matches float
-Some(1)
-Checking if float matches double
+false
 None
-Checking if class java.lang.Float matches float
+true
 Some(1)
-Checking if double matches double
-Some(1)
-Checking if double matches boolean
+false
 None
-Checking if class java.lang.Double matches double
-Some(1)
-Checking if boolean matches boolean
+true
 Some(true)
-Checking if boolean matches void
+false
 None
-Checking if class java.lang.Boolean matches boolean
-Some(true)
-Checking if void matches void
+true
 Some(undefined)
-Checking if void matches byte
+false
 None
-Checking if class scala.runtime.BoxedUnit matches void
-Some(undefined)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.0/run/virtpatmat_switch.check
@@ -1,0 +1,7 @@
+zero
+one
+many
+got a
+got b
+got some letter
+scala.MatchError: 5 (of class java.lang.Byte)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.0/run/virtpatmat_typetag.check
@@ -1,0 +1,10 @@
+1 is not a Int; it's a class java.lang.Byte
+1 is a java.lang.Integer
+1 is not a java.lang.String; it's a class java.lang.Byte
+true is a Any
+woele is a java.lang.String
+1 is not a Int; it's a class java.lang.Byte
+1 is a java.lang.Integer
+1 is not a java.lang.String; it's a class java.lang.Byte
+true is a Any
+woele is a java.lang.String

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.1/run/t5568.check
@@ -1,0 +1,9 @@
+void
+int
+class scala.runtime.BoxedUnit
+class scala.runtime.BoxedUnit
+class java.lang.Byte
+class java.lang.Byte
+5
+5
+5

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.1/run/t5629b.check
@@ -1,0 +1,10 @@
+=== pf(1):
+MySmartPF.apply entered...
+newPF.applyOrElse entered...
+default
+scala.MatchError: 1 (of class java.lang.Byte)
+=== pf(42):
+MySmartPF.apply entered...
+newPF.applyOrElse entered...
+ok
+=== done

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.1/run/t6318_primitives.check
@@ -1,54 +1,36 @@
-Checking if byte matches byte
+true
 Some(1)
-Checking if byte matches short
+false
 None
-Checking if class java.lang.Integer matches byte
+true
 Some(1)
-Checking if short matches short
-Some(1)
-Checking if short matches char
+false
 None
-Checking if class java.lang.Short matches short
-Some(1)
-Checking if char matches char
+true
 Some()
-Checking if char matches int
+false
 None
-Checking if class java.lang.Character matches char
-Some()
-Checking if int matches int
+true
 Some(1)
-Checking if int matches long
+false
 None
-Checking if class java.lang.Integer matches int
+true
 Some(1)
-Checking if long matches long
-Some(1)
-Checking if long matches float
+false
 None
-Checking if class java.lang.Long matches long
+true
 Some(1)
-Checking if float matches float
-Some(1)
-Checking if float matches double
+false
 None
-Checking if class java.lang.Float matches float
+true
 Some(1)
-Checking if double matches double
-Some(1)
-Checking if double matches boolean
+false
 None
-Checking if class java.lang.Double matches double
-Some(1)
-Checking if boolean matches boolean
+true
 Some(true)
-Checking if boolean matches void
+false
 None
-Checking if class java.lang.Boolean matches boolean
-Some(true)
-Checking if void matches void
+true
 Some(undefined)
-Checking if void matches byte
+false
 None
-Checking if class scala.runtime.BoxedUnit matches void
-Some(undefined)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.1/run/virtpatmat_switch.check
@@ -1,0 +1,7 @@
+zero
+one
+many
+got a
+got b
+got some letter
+scala.MatchError: 5 (of class java.lang.Byte)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.1/run/virtpatmat_typetag.check
@@ -1,0 +1,10 @@
+1 is not a Int; it's a class java.lang.Byte
+1 is a java.lang.Integer
+1 is not a java.lang.String; it's a class java.lang.Byte
+true is a Any
+woele is a java.lang.String
+1 is not a Int; it's a class java.lang.Byte
+1 is a java.lang.Integer
+1 is not a java.lang.String; it's a class java.lang.Byte
+true is a Any
+woele is a java.lang.String

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.2/run/t5568.check
@@ -1,0 +1,9 @@
+void
+int
+class scala.runtime.BoxedUnit
+class scala.runtime.BoxedUnit
+class java.lang.Byte
+class java.lang.Byte
+5
+5
+5

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.2/run/t5629b.check
@@ -1,0 +1,10 @@
+=== pf(1):
+MySmartPF.apply entered...
+newPF.applyOrElse entered...
+default
+scala.MatchError: 1 (of class java.lang.Byte)
+=== pf(42):
+MySmartPF.apply entered...
+newPF.applyOrElse entered...
+ok
+=== done

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.2/run/t6318_primitives.check
@@ -1,14 +1,14 @@
 Checking if byte matches byte
 Some(1)
 Checking if byte matches short
-None
-Checking if class java.lang.Integer matches byte
+Some(1)
+Checking if class java.lang.Byte matches byte
 Some(1)
 Checking if short matches short
 Some(1)
 Checking if short matches char
 None
-Checking if class java.lang.Short matches short
+Checking if class java.lang.Byte matches short
 Some(1)
 Checking if char matches char
 Some()
@@ -20,7 +20,7 @@ Checking if int matches int
 Some(1)
 Checking if int matches long
 None
-Checking if class java.lang.Integer matches int
+Checking if class java.lang.Byte matches int
 Some(1)
 Checking if long matches long
 Some(1)
@@ -31,14 +31,14 @@ Some(1)
 Checking if float matches float
 Some(1)
 Checking if float matches double
-None
-Checking if class java.lang.Float matches float
+Some(1)
+Checking if class java.lang.Byte matches float
 Some(1)
 Checking if double matches double
 Some(1)
 Checking if double matches boolean
 None
-Checking if class java.lang.Double matches double
+Checking if class java.lang.Byte matches double
 Some(1)
 Checking if boolean matches boolean
 Some(true)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.2/run/virtpatmat_switch.check
@@ -1,0 +1,7 @@
+zero
+one
+many
+got a
+got b
+got some letter
+scala.MatchError: 5 (of class java.lang.Byte)

partest-suite/src/test/resources/scala/tools/partest/scalajs/2.11.2/run/virtpatmat_typetag.check
@@ -1,0 +1,10 @@
+1 is a Int
+1 is a java.lang.Integer
+1 is not a java.lang.String; it's a class java.lang.Byte
+true is a Any
+woele is a java.lang.String
+1 is a Int
+1 is a java.lang.Integer
+1 is not a java.lang.String; it's a class java.lang.Byte
+true is a Any
+woele is a java.lang.String

test-suite/src/test/scala/scala/scalajs/testsuite/compiler/ReflectionTest.scala
@@ -44,12 +44,12 @@ object ReflectionTest extends JasmineTest {
         c.asInstanceOf[js.Any]
       expect((false: Any).getClass).toBe(classOf[java.lang.Boolean])
       expect(('a': Any).getClass).toBe(classOf[java.lang.Character])
-      expect((1.toByte: Any).getClass).toBe(classOf[java.lang.Integer])
-      expect((1.toShort: Any).getClass).toBe(classOf[java.lang.Integer])
-      expect((1: Any).getClass).toBe(classOf[java.lang.Integer])
+      expect((1.toByte: Any).getClass).toBe(classOf[java.lang.Byte])
+      expect((1.toShort: Any).getClass).toBe(classOf[java.lang.Byte])
+      expect((1: Any).getClass).toBe(classOf[java.lang.Byte])
       expect((1L: Any).getClass).toBe(classOf[java.lang.Long])
-      expect((1.5f: Any).getClass).toBe(classOf[java.lang.Double])
-      expect((1.5: Any).getClass).toBe(classOf[java.lang.Double])
+      expect((1.5f: Any).getClass).toBe(classOf[java.lang.Float])
+      expect((1.5: Any).getClass).toBe(classOf[java.lang.Float])
       expect(((): Any).getClass).toBe(classOf[scala.runtime.BoxedUnit])
     }
 