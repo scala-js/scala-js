ed975d14be928673bf22e4559547ef90a7e6774c
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-11-20 00:40:24 +0100

    Cleanup java.lang.ref.*.

--- javalanglib/src/main/scala/java/lang/ref/PhantomReference.scala
+++ javalanglib/src/main/scala/java/lang/ref/PhantomReference.scala
@@ -1,7 +1,7 @@
 package java.lang.ref
 
+class PhantomReference[T >: Null <: AnyRef](referent: T,
+    queue: ReferenceQueue[_ >: T]) extends Reference[T](null) {
 
-class PhantomReference[T >: Null <: AnyRef](referent: T, queue: ReferenceQueue[_]) extends Reference[T](referent) {
-  def this(referent: T) = this(referent, null)
-  override def get: T = null
+  override def get(): T = null
 }
