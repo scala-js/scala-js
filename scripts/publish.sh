e17be1cd338c8261f53bcd7399cdbd64bb986cf6
Sébastien Doeraene <sjrdoeraene@gmail.com>
2014-11-28 13:54:39 +0100

    Fix #1318: Configuration to publish to OSS Sonatype.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,10 +2,10 @@
 
 if [ $# -eq 1 -a "$1" = "-x" ]; then
     export PUBLISH_TO_BINTRAY=true
-    CMD=sbt
+    CMD="sbt"
 else
-    echo "Showing commands that would be issued to SBT. Use -x to run"
-    CMD=cat
+    echo "Showing commands that would be executed. Use -x to run."
+    CMD="echo sbt"
 fi
 
 FULL_VERSIONS="2.10.2 2.10.3 2.10.4 2.11.0 2.11.1 2.11.2 2.11.4"
@@ -15,21 +15,20 @@ SBT_VERSION="2.10.4"
 LIBS="library javalibEx ir irJS tools toolsJS jsEnvs testAdapter stubs testInterface"
 
 # Publish compiler
+ARGS=""
 for v in $FULL_VERSIONS; do
-    echo "++$v"
-    echo "compiler/publish"
-done | $CMD
+    ARGS="$ARGS ++$v compiler/publishSigned"
+done
+$CMD $ARGS
 
 # Package libraries
-for p in $LIBS; do
-    for v in $BIN_VERSIONS; do
-        echo "++$v"
-        echo "$p/publish"
-    done | $CMD
+for v in $BIN_VERSIONS; do
+    ARGS="++$v"
+    for p in $LIBS; do
+        ARGS="$ARGS $p/publishSigned"
+    done
+    $CMD $ARGS
 done
 
 # Publish sbt-plugin
-(
-    echo "++$SBT_VERSION"
-    echo "sbtPlugin/publish"
-) | $CMD
+$CMD "++$SBT_VERSION" "sbtPlugin/publishSigned"
