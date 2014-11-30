6aa2d17a446cd10cce00927dc5d8f8d38de9855a
Tobias Schlatter <tobias@meisch.ch>
2014-11-30 16:02:11 +0100

    Fix #1332: Correct package names for CLI runners
    This was forgotten in the refactoring commit:
    3200c872b1f567393eaf6632048b2c21a1154c11

--- cli/src/main/resources/scalajsp.bat
+++ cli/src/main/resources/scalajsp.bat
@@ -4,4 +4,4 @@ set SCALAJS_VER=@SCALAJS_VER@
 
 set CLILIB="%~dp0\..\lib\scalajs-cli-assembly_%SCALA_BIN_VER%-%SCALAJS_VER%.jar"
 
-scala -classpath %CLILIB% scala.scalajs.cli.Scalajsp %*
+scala -classpath %CLILIB% org.scalajs.cli.Scalajsp %*
