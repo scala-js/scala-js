90173744f5bd0fc7553abf0759f8c5e34f22a0c5
Tobias Schlatter <tobias@meisch.ch>
2014-12-03 08:48:46 +0100

    Only fail JSEnv test if log is bad

--- js-envs/src/test/scala/org/scalajs/jsenv/test/JSEnvTest.scala
+++ js-envs/src/test/scala/org/scalajs/jsenv/test/JSEnvTest.scala
@@ -4,9 +4,11 @@ import org.scalajs.jsenv._
 
 import org.scalajs.core.tools.io.MemVirtualJSFile
 import org.scalajs.core.tools.classpath.PartialClasspath
-import org.scalajs.core.tools.logging.NullLogger
+import org.scalajs.core.tools.logging._
 
 import org.junit.Assert._
+
+import StoreLogger._
 
 abstract class JSEnvTest {
 
@@ -25,9 +27,14 @@ abstract class JSEnvTest {
       newJSEnv.jsRunner(emptyCP, code, logger, console).run()
 
       val log = logger.getLog
+      val hasBadLog = log exists {
+        case Log(level, _) if level >= Level.Warn => true
+        case Trace(_) => true
+        case _ => false
+      }
 
-      assertTrue("VM shouldn't produce log. Log:\n" +
-          log.mkString("\n"), log.isEmpty)
+      assertFalse("VM shouldn't log errors, warnings or traces. Log:\n" +
+          log.mkString("\n"), hasBadLog)
       assertEquals("Output should match", expectedOut, console.getLog)
     }
 
