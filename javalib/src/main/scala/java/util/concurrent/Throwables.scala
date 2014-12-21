5ffdcf69fc316d747dcdba2a1dc68f57591172b3
Alexandru Nedelcu <alexandru@nedelcu.net>
2014-12-21 19:08:30 +0200

    Fix #1406: Add TimeoutException and CancellationException

--- javalib/src/main/scala/java/util/concurrent/Throwables.scala
+++ javalib/src/main/scala/java/util/concurrent/Throwables.scala
@@ -7,3 +7,13 @@ class ExecutionException(message: String, cause: Throwable)
   protected def this(message: String) = this(message, null)
   def this(cause: Throwable) = this(null, cause)
 }
+
+class CancellationException(message: String)
+    extends IllegalStateException(message) {
+
+  def this() = this(null)
+}
+
+class TimeoutException(message: String) extends Exception(message) {
+  def this() = this(null)
+}
