8cf4a14ff4970a952bd91447cd8ddaf7385e4991
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-03-11 15:52:19 +0100

    Fix #1526: Implement java.lang.Thread.{get,set}Name.

--- javalanglib/src/main/scala/java/lang/Thread.scala
+++ javalanglib/src/main/scala/java/lang/Thread.scala
@@ -6,7 +6,15 @@ package java.lang
  * So we use a binary signature that no Java source file can ever produce.
  */
 class Thread private (dummy: Unit) extends Runnable {
+  private[this] var name: String = "main" // default name of the main thread
+
   def run(): Unit = ()
+
+  final def setName(name: String): Unit =
+    this.name = name
+
+  final def getName(): String =
+    this.name
 
   def getStackTrace(): Array[StackTraceElement] =
     scala.scalajs.runtime.StackTrace.getCurrentStackTrace()
--- test-suite/src/test/scala/org/scalajs/testsuite/javalib/ThreadTest.scala
+++ test-suite/src/test/scala/org/scalajs/testsuite/javalib/ThreadTest.scala
@@ -14,6 +14,18 @@ import org.scalajs.jasminetest.JasmineTest
 object ThreadTest extends JasmineTest {
 
   describe("java.lang.Thread") {
+    it("getName and setName") {
+      val t = Thread.currentThread()
+      expect(t.getName).toBe("main") // default name of the main thread
+      t.setName("foo")
+      try {
+        expect(t.getName).toBe("foo")
+      } finally {
+        t.setName("main") // don't pollute the rest of the world with this test
+      }
+      expect(t.getName).toBe("main")
+    }
+
     it("Thread.currentThread().getStackTrace() should exist and not crash") {
       java.lang.Thread.currentThread().getStackTrace()
     }
