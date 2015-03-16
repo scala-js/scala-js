824ac40a4fc68143ff9112f39310d9cff12ef8e6
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-03-16 17:53:02 +0100

    Version 0.6.2.

--- ir/src/main/scala/org/scalajs/core/ir/ScalaJSVersions.scala
+++ ir/src/main/scala/org/scalajs/core/ir/ScalaJSVersions.scala
@@ -3,7 +3,7 @@ package org.scalajs.core.ir
 object ScalaJSVersions {
 
   /** the Scala.js version of this build */
-  final val current = "0.6.2-SNAPSHOT"
+  final val current = "0.6.2"
 
   /** true iff the Scala.js version of this build is a snapshot version. */
   final val currentIsSnapshot = current endsWith "-SNAPSHOT"
