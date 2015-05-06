1008a8fb9413c332fac598ed7840fe3e01cb9a8a
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-05-06 14:44:08 +0200

    Add two core reachable symbols that are missing in theory.
    In practice, it does not show up as a user error, because if the
    helpers calling them are only reachable if some other method is
    reachable that also makes the symbols reachable.
    
    But it becomes an early undeclared variable in strong mode.

--- tools/shared/src/main/scala/org/scalajs/core/tools/optimizer/Analyzer.scala
+++ tools/shared/src/main/scala/org/scalajs/core/tools/optimizer/Analyzer.scala
@@ -99,6 +99,7 @@ final class Analyzer(semantics: Semantics, outputMode: OutputMode,
     ObjectClass.callMethod("equals__O__Z")
 
     instantiateClassWith("jl_NullPointerException", "init___")
+    instantiateClassWith("jl_CloneNotSupportedException", "init___")
 
     if (asInstanceOfs != Unchecked)
       instantiateClassWith("jl_ClassCastException", "init___T")
@@ -120,6 +121,7 @@ final class Analyzer(semantics: Semantics, outputMode: OutputMode,
     RTStringModuleClass.callMethod("hashCode__T__I")
 
     val RTLongClass = lookupClass(LongImpl.RuntimeLongClass)
+    RTLongClass.useInstanceTests()
     RTLongClass.instantiated()
     for (method <- LongImpl.AllConstructors)
       RTLongClass.callMethod(method, statically = true)
