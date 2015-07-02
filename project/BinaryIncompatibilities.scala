b46fd106adb2d034956fee8a75a301cd346e892a
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-07-02 22:43:32 +0200

    Towards 0.6.5.

--- project/BinaryIncompatibilities.scala
+++ project/BinaryIncompatibilities.scala
@@ -3,37 +3,15 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object BinaryIncompatibilities {
   val IR = Seq(
-      // Breaking: UndefinedParam does not inherit from Literal anymore
-      ProblemFilters.exclude[MissingTypesProblem](
-          "org.scalajs.core.ir.Trees$UndefinedParam")
   )
 
   val Tools = Seq(
-      // Private things, not an issue
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.optimizer.OptimizerCore.org$scalajs$core$tools$optimizer$OptimizerCore$$shouldInlineBecauseOfArgs"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.optimizer.OptimizerCore.org$scalajs$core$tools$optimizer$OptimizerCore$$treeNotInlined0$2"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.optimizer.OptimizerCore.org$scalajs$core$tools$optimizer$OptimizerCore$$treeNotInlined0$3"),
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.optimizer.OptimizerCore.org$scalajs$core$tools$optimizer$OptimizerCore$$inline"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.optimizer.OptimizerCore.org$scalajs$core$tools$optimizer$OptimizerCore$$exceptionMsg"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.optimizer.Emitter.org$scalajs$core$tools$optimizer$Emitter$$ClassesWhoseDataReferToTheirInstanceTests")
   )
 
   val JSEnvs = Seq(
-      // Private things, not an issue
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.jsenv.rhino.ScalaJSCoreLib.org$scalajs$jsenv$rhino$ScalaJSCoreLib$$loadAncestors")
   )
 
   val SbtPlugin = Seq(
-      // Potentially breaking, but no one calls this method except through polymorphism
-      ProblemFilters.exclude[IncompatibleResultTypeProblem](
-          "org.scalajs.sbtplugin.ScalaJSPlugin.requires")
   )
 
   val TestAdapter = Seq(
--- project/Build.scala
+++ project/Build.scala
@@ -45,7 +45,7 @@ object Build extends sbt.Build {
   val shouldPartest = settingKey[Boolean](
     "Whether we should partest the current scala version (and fail if we can't)")
 
-  val previousVersion = "0.6.3"
+  val previousVersion = "0.6.4"
   val previousSJSBinaryVersion =
     ScalaJSCrossVersion.binaryScalaJSVersion(previousVersion)
   val previousBinaryCrossVersion =
@@ -761,8 +761,7 @@ object Build extends sbt.Build {
               "com.github.scopt" %% "scopt" % "3.2.0"
           ),
 
-          // TODO Enable this when going towards 0.6.5:
-          //previousArtifactSetting,
+          previousArtifactSetting,
 
           // assembly options
           mainClass in assembly := None, // don't want an executable JAR
