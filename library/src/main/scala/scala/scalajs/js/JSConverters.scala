c6bbf549f92264dd93f7a31a904bc9b15c6bb2c1
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-01-29 14:56:39 +0100

    Add js.Promise and js.Thenable from ECMAScript 6.
    There are implicit conversions from Future to Thenable and back.

--- library/src/main/scala/scala/scalajs/js/JSConverters.scala
+++ library/src/main/scala/scala/scalajs/js/JSConverters.scala
@@ -11,14 +11,25 @@ package scala.scalajs.js
 
 import scala.language.implicitConversions
 
+import scala.scalajs.js
+
 import scala.collection._
+import scala.concurrent.{ExecutionContext, Future}
 
 import scala.scalajs.runtime.genTraversableOnce2jsArray
+
+sealed abstract class JSConvertersLowPrioImplicits { this: JSConverters.type =>
+
+  @inline
+  implicit def JSRichFutureNonThenable[A](f: Future[A]): JSRichFuture[A] =
+    new JSRichFuture[A](f.asInstanceOf[Future[A | Thenable[A]]])
+
+}
 
 /** A collection of decorators that allow converting Scala types to
  *  corresponding JS facade types
  */
-object JSConverters {
+object JSConverters extends JSConvertersLowPrioImplicits {
 
   implicit class JSRichOption[T](val opt: Option[T]) extends AnyVal {
     @inline final def orUndefined: UndefOr[T] =
@@ -50,5 +61,36 @@ object JSConverters {
   implicit def array2JSRichGenTrav[T](
       arr: scala.Array[T]): JSRichGenTraversableOnce[T] =
     new JSRichGenTraversableOnce(arr)
+
+  @inline
+  implicit def JSRichFutureThenable[A](f: Future[Thenable[A]]): JSRichFuture[A] =
+    new JSRichFuture[A](f.asInstanceOf[Future[A | Thenable[A]]])
+
+  final class JSRichFuture[A](val self: Future[A | Thenable[A]]) extends AnyVal {
+    /** Converts the Future to a JavaScript [[Promise]].
+     *
+     *  Attention! The nature of the [[Promise]] class, from the ECMAScript
+     *  specification, makes this method inherently un-typeable, because it is
+     *  not type parametric.
+     *
+     *  The signature of the `toJSPromise` method is only valid
+     *  <i>provided that</i> the values of `A` do not have a `then` method.
+     */
+    def toJSPromise(implicit executor: ExecutionContext): Promise[A] = {
+      new Promise[A]({
+        (resolve: js.Function1[A | Thenable[A], _], reject: js.Function1[scala.Any, _]) =>
+          self onComplete {
+            case scala.util.Success(value) =>
+              resolve(value)
+
+            case scala.util.Failure(th) =>
+              reject(th match {
+                case JavaScriptException(e) => e
+                case _                      => th
+              })
+          }
+      })
+    }
+  }
 
 }
---
+++ library/src/main/scala/scala/scalajs/js/Thenable.scala
@@ -0,0 +1,83 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___      __ ____  Scala.js API               **
+**    / __/ __// _ | / /  / _ | __ / // __/  (c) 2013-2015, LAMP/EPFL   **
+**  __\ \/ /__/ __ |/ /__/ __ |/_// /_\ \    http://scala-js.org/       **
+** /____/\___/_/ |_/____/_/ | |__/ /____/                               **
+**                          |/____/                                     **
+\*                                                                      */
+
+
+package scala.scalajs.js
+
+import scala.language.implicitConversions
+
+import scala.scalajs.js
+import js.annotation._
+
+import scala.concurrent.Future
+
+/** A thing on which one can call the `then` method.
+ *
+ *  `Thenable`s are automatically transitively flattened by the `then` method
+ *  of `Thenable`s. In particular, this is true for [[Promise]]s.
+ *
+ *  Attention! The nature of this interface, from the ECMAScript specification,
+ *  makes it inherently un-typeable, because it is not type parametric.
+ *
+ *  The signature of the `then` method is only valid <i>provided that</i> the
+ *  values of `B` do not have a `then` method.
+ */
+@ScalaJSDefined
+trait Thenable[+A] extends js.Object {
+  def `then`[B](
+      onFulfilled: js.Function1[A, B | Thenable[B]],
+      onRejected: js.UndefOr[js.Function1[scala.Any, B | Thenable[B]]]): Thenable[B]
+
+  def `then`[B >: A](
+      onFulfilled: Unit,
+      onRejected: js.UndefOr[js.Function1[scala.Any, B | Thenable[B]]]): Thenable[B]
+}
+
+object Thenable {
+  implicit class ThenableOps[+A](val p: Thenable[A]) extends AnyVal {
+    /** Converts the [[Thenable]] into a Scala [[scala.concurrent.Future Future]].
+     *
+     *  Unlike when calling the `then` methods of [[Thenable]], the resulting
+     *  [[scala.concurrent.Future Future]] is always properly typed, and
+     *  operations on it will be well-typed in turn.
+     */
+    def toFuture: Future[A] = {
+      // Help for inference
+      def defined[A](x: A): js.UndefOr[A] = x
+
+      val p2 = scala.concurrent.Promise[A]()
+      p.`then`[Unit](
+          { (v: A) =>
+            p2.success(v)
+            (): Unit | Thenable[Unit]
+          },
+          defined { (e: scala.Any) =>
+            p2.failure(e match {
+              case th: Throwable => th
+              case _             => JavaScriptException(e)
+            })
+            (): Unit | Thenable[Unit]
+          })
+      p2.future
+    }
+  }
+
+  /** Implicits for [[Thenable]]s.
+   *
+   *  Import `Implicits._` to enable [[scala.concurrent.Future Future]]'s
+   *  operations directly on [[Thenable]]s, without needing to convert them
+   *  with [[ThenableOps.toFuture toFuture]].
+   *
+   *  Unlike the `then` methods in [[Thenable]],
+   *  [[scala.concurrent.Future Future]]'s operations are always well-typed.
+   */
+  object Implicits {
+    implicit def thenable2future[A](p: Thenable[A]): Future[A] =
+      p.toFuture
+  }
+}
