72363b34c441fc5fbeb15dbb37bceac65e6a68a2
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-02-11 13:49:55 +0100

    Towards 0.6.8.

--- project/BinaryIncompatibilities.scala
+++ project/BinaryIncompatibilities.scala
@@ -6,21 +6,6 @@ object BinaryIncompatibilities {
   )
 
   val Tools = Seq(
-      // private, not an issue
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.frontend.optimizer.OptimizerCore#RollbackException.this"),
-
-      // private[emitter], not an issue
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring$"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring#DesugarException.this"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring#Env.this"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring#JSDesugar.this"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring#MyTreeOps.this")
   )
 
   val JSEnvs = Seq(
@@ -36,17 +21,8 @@ object BinaryIncompatibilities {
   )
 
   val Library = Seq(
-      // private[concurrent], not an issue
-      ProblemFilters.exclude[MissingTypesProblem](
-          "scala.scalajs.concurrent.QueueExecutionContext$"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "scala.scalajs.concurrent.QueueExecutionContext.reportFailure"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "scala.scalajs.concurrent.QueueExecutionContext.execute"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "scala.scalajs.concurrent.QueueExecutionContext.prepare")
   )
 
   val TestInterface = Seq(
-    )
+  )
 }
