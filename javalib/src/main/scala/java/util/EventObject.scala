3e688ed4b4af15074f0c063549103424550e9d28
杨博 (Yang Bo) <pop.atry@gmail.com>
2016-05-15 22:40:43 +0800

    Add the toString method

--- javalib/src/main/scala/java/util/EventObject.scala
+++ javalib/src/main/scala/java/util/EventObject.scala
@@ -1,5 +1,9 @@
 package java.util
 
 class EventObject(protected var source: AnyRef) extends java.io.Serializable {
+  
   def getSource(): AnyRef = source
+  
+  override def toString(): String = getClass.getName + "[source=" + source + "]"
+
 }
