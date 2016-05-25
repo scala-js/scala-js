9d933450ebe8ffe0f155efaa4fd16a1d3a88634a
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-05-25 13:58:07 +0200

    Fix #2412: Pull changes in ranges toString format in 2.12.
    From commit 214ea82573624bffb4d0f16f3e5c49f9370ba7a7

--- test-suite/shared/src/test/scala/org/scalajs/testsuite/scalalib/RangesTest.scala
+++ test-suite/shared/src/test/scala/org/scalajs/testsuite/scalalib/RangesTest.scala
@@ -55,4 +55,39 @@ class RangesTest {
     assertEquals(512, r.length)
     assertEquals(Int.MinValue, r.sum)
   }
+
+  @Test def Range_toString_issue_2412(): Unit = {
+    if (scalaVersion.startsWith("2.12.") &&
+        // Remove this line when upgrading to 2.12.0-M5
+        !scalaVersion.startsWith("2.12.0-M4")) {
+      assertEquals("inexact Range 1 to 10 by 2", (1 to 10 by 2).toString)
+      assertEquals("empty Range 1 until 1 by 2", (1 until 1 by 2).toString)
+      assertEquals("Range requires step", (0.0 to 1.0).toString)
+      assertEquals("Range 0 to 1", (0 to 1).toString)
+    } else {
+      assertEquals("Range(1, 3, 5, 7, 9)", (1 to 10 by 2).toString)
+      assertEquals("Range()", (1 until 1 by 2).toString)
+      assertTrue((0.0 to 1.0).toString.startsWith("scala.collection.immutable.Range$Partial"))
+      assertEquals("Range(0, 1)", (0 to 1).toString)
+    }
+  }
+
+  @Test def NumericRange_toString_issue_2412(): Unit = {
+    if (scalaVersion.startsWith("2.12.") &&
+        // Remove this line when upgrading to 2.12.0-M5
+        !(executingInJVM && scalaVersion.startsWith("2.12.0-M4"))) {
+      assertEquals(s"NumericRange 0.1 to ${1.0} by 0.1",
+          (0.1 to 1.0 by 0.1).toString())
+      assertEquals(
+          s"NumericRange 0.1 until ${1.0} by 0.1 (using NumericRange 0.1 until ${1.0} by 0.1 of BigDecimal)",
+          Range.Double(0.1, 1.0, 0.1).toString)
+    } else {
+      assertEquals("NumericRange(0.1, 0.2, 0.30000000000000004, 0.4, 0.5, 0.6, 0.7, " +
+          "0.7999999999999999, 0.8999999999999999, 0.9999999999999999)",
+          (0.1 to 1.0 by 0.1).toString())
+      assertEquals(
+          "NumericRange(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)",
+          Range.Double(0.1, 1.0, 0.1).toString)
+    }
+  }
 }
