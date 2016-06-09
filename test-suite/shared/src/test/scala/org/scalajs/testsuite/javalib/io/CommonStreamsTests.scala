02970f42cbb1891194c5d996a17aa276ad211d40
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-06-09 11:42:11 +0200

    Fix #2461: Fix tests tait javalib.io.CommonStreamsTests.

--- test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/io/CommonStreamsTests.scala
+++ test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/io/CommonStreamsTests.scala
@@ -18,145 +18,147 @@ import org.scalajs.testsuite.utils.AssertThrows._
 
 trait CommonStreamsTests {
 
-  def byteArrayInputStreamLikeTests(mkStream: Seq[Int] => InputStream): Unit = {
-    val length = 50
-    def newStream = mkStream(1 to length)
+  def mkStream(seq: Seq[Int]): InputStream
 
-    @Test def should_provide_read()(): Unit = {
-      val stream = newStream
+  private val length = 50
+  private def newStream: InputStream = mkStream(1 to length)
 
-      for (i <- 1 to length)
-        assertEquals(i, stream.read())
+  private implicit def seqToArray(seq: Seq[Int]): Array[Byte] =
+    seq.toArray.map(_.toByte)
 
-      for (_ <- 1 to 5)
-        assertEquals(-1, stream.read())
-    }
+  @Test def should_provide_read()(): Unit = {
+    val stream = newStream
 
-    @Test def should_provide_read_from_buf(): Unit = {
-      val stream = newStream
-      val buf = new Array[Byte](10)
+    for (i <- 1 to length)
+      assertEquals(i, stream.read())
 
-      assertEquals(10, stream.read(buf))
-      assertEquals(1 to 10, buf)
-
-      assertEquals(35, stream.skip(35))
-
-      assertEquals(5, stream.read(buf))
-      assertEquals((46 to 50) ++ (6 to 10), buf)
-
-      assertEquals(-1, stream.read(buf))
+    for (_ <- 1 to 5)
       assertEquals(-1, stream.read())
+  }
+
+  @Test def should_provide_read_from_buf(): Unit = {
+    val stream = newStream
+    val buf = new Array[Byte](10)
+
+    assertEquals(10, stream.read(buf))
+    assertArrayEquals(1 to 10, buf)
+
+    assertEquals(35L, stream.skip(35))
+
+    assertEquals(5, stream.read(buf))
+    assertArrayEquals((46 to 50) ++ (6 to 10), buf)
+
+    assertEquals(-1, stream.read(buf))
+    assertEquals(-1, stream.read())
+  }
+
+  @Test def should_provide_full_argument_read(): Unit = {
+    val stream = newStream
+    val buf = new Array[Byte](20)
+
+    assertEquals(5, stream.read(buf, 10, 5))
+    assertArrayEquals(Seq.fill(10)(0) ++ (1 to 5) ++ Seq.fill(5)(0), buf)
+
+    assertEquals(20, stream.read(buf, 0, 20))
+    assertArrayEquals(6 to 25, buf)
+
+    assertEquals(0, stream.read(buf, 10, 0))
+    assertArrayEquals(6 to 25, buf)
+
+    expectThrows(classOf[IndexOutOfBoundsException], stream.read(buf, -1, 0))
+    expectThrows(classOf[IndexOutOfBoundsException], stream.read(buf, 0, -1))
+    expectThrows(classOf[IndexOutOfBoundsException], stream.read(buf, 100, 0))
+    expectThrows(classOf[IndexOutOfBoundsException], stream.read(buf, 10, 100))
+    assertArrayEquals(6 to 25, buf)
+
+    assertEquals(20L, stream.skip(20))
+
+    assertEquals(5, stream.read(buf, 0, 10))
+    assertArrayEquals((46 to 50) ++ (11 to 25), buf)
+
+    assertEquals(-1, stream.read(buf, 0, 10))
+    assertArrayEquals((46 to 50) ++ (11 to 25), buf)
+
+  }
+
+  @Test def should_provide_available(): Unit = {
+    val stream = newStream
+
+    def mySkip(n: Int) = for (_ <- 1 to n) assertNotEquals(stream.read(), -1)
+    def check(n: Int) = assertEquals(n, stream.available)
+
+    check(50)
+    mySkip(5)
+    check(45)
+    assertEquals(10L, stream.skip(10))
+    check(35)
+    mySkip(30)
+    check(5)
+    assertEquals(5L, stream.skip(20))
+    check(0)
+  }
+
+  @Test def should_provide_skip(): Unit = {
+    val stream = newStream
+
+    assertEquals(7L, stream.skip(7))
+
+    for (i <- 8 to 32)
+      assertEquals(i, stream.read())
+
+    assertEquals(0L, stream.skip(0))
+    assertEquals(33, stream.read())
+    assertEquals(0L, stream.skip(-4))
+    assertEquals(34, stream.read())
+
+    assertEquals(16L, stream.skip(30))
+    assertEquals(0L, stream.skip(30))
+  }
+
+  @Test def should_return_true_from_markSupported(): Unit = {
+    assertTrue(newStream.markSupported)
+  }
+
+  @Test def should_provide_no_op_close(): Unit = {
+    val stream = newStream
+
+    for (i <- 1 to length) {
+      stream.close()
+      assertEquals(i, stream.read())
     }
+  }
 
-    @Test def should_provide_full_argument_read(): Unit = {
-      val stream = newStream
-      val buf = new Array[Byte](20)
+  @Test def should_provide_mark_and_reset(): Unit = {
+    val stream = newStream
 
-      assertEquals(5, stream.read(buf, 10, 5))
-      assertEquals(Seq.fill(10)(0) ++ (1 to 5) ++ Seq.fill(5)(0), buf)
+    def read(range: Range) = for (i <- range) assertEquals(i, stream.read())
 
-      assertEquals(20, stream.read(buf, 0, 20))
-      assertEquals(6 to 25, buf)
+    read(1 to 10)
+    stream.reset() // mark must be 0 at creation
+    read(1 to 5)
+    stream.mark(length)
+    read(6 to 22)
+    stream.reset()
+    read(6 to 20)
+    stream.reset()
+    read(6 to 25)
+    stream.reset()
+    assertEquals(40L, stream.skip(40))
+    stream.mark(length)
+    read(46 to 50)
+    stream.reset()
+    read(46 to 50)
+    stream.mark(length)
+    assertEquals(-1, stream.read())
+    stream.reset()
+    assertEquals(-1, stream.read())
+  }
 
-      assertEquals(0, stream.read(buf, 10, 0))
-      assertEquals(6 to 25, buf)
-
-      expectThrows(classOf[IOException], stream.read(buf, -1, 0))
-      expectThrows(classOf[IOException], stream.read(buf, 0, -1))
-      expectThrows(classOf[IOException], stream.read(buf, 100, 0))
-      expectThrows(classOf[IOException], stream.read(buf, 10, 100))
-      assertEquals(6 to 25, buf)
-
-      assertEquals(20, stream.skip(20))
-
-      assertEquals(5, stream.read(buf, 0, 10))
-      assertEquals((46 to 50) ++ (11 to 25), buf)
-
-      assertEquals(-1, stream.read(buf, 0, 10))
-      assertEquals(0, stream.read(buf, 0, 0))
-      assertEquals((46 to 50) ++ (11 to 25), buf)
-
-    }
-
-    @Test def should_provide_available(): Unit = {
-      val stream = newStream
-
-      def mySkip(n: Int) = for (_ <- 1 to n) assertNotEquals(stream.read(), -1)
-      def check(n: Int) = assertEquals(n, stream.available)
-
-      check(50)
-      mySkip(5)
-      check(45)
-      assertEquals(10, stream.skip(10))
-      check(35)
-      mySkip(30)
-      check(5)
-      assertEquals(5, stream.skip(20))
-      check(0)
-    }
-
-    @Test def should_provide_skip(): Unit = {
-      val stream = newStream
-
-      assertEquals(7, stream.skip(7))
-
-      for (i <- 8 to 32)
-        assertEquals(i, stream.read())
-
-      assertEquals(0, stream.skip(0))
-      assertEquals(33, stream.read())
-      assertEquals(0, stream.skip(-4))
-      assertEquals(34, stream.read())
-
-      assertEquals(16, stream.skip(30))
-      assertEquals(0, stream.skip(30))
-    }
-
-    @Test def should_return_true_from_markSupported(): Unit = {
-      assertTrue(newStream.markSupported)
-    }
-
-    @Test def should_provide_no_op_close(): Unit = {
-      val stream = newStream
-
-      for (i <- 1 to length) {
-        stream.close()
-        assertEquals(i, stream.read())
-      }
-    }
-
-    @Test def should_provide_mark_and_reset(): Unit = {
-      val stream = newStream
-
-      def read(range: Range) = for (i <- range) assertEquals(i, stream.read())
-
-      read(1 to 10)
-      stream.reset() // mark must be 0 at creation
-      read(1 to 5)
-      stream.mark(length)
-      read(6 to 22)
-      stream.reset()
-      read(6 to 20)
-      stream.reset()
-      read(6 to 25)
-      stream.reset()
-      assertEquals(40, stream.skip(40))
-      stream.mark(length)
-      read(46 to 50)
-      stream.reset()
-      read(46 to 50)
-      stream.mark(length)
-      assertEquals(-1, stream.read())
-      stream.reset()
-      assertEquals(-1, stream.read())
-    }
-
-    @Test def should_return_positive_integers_when_calling_read(): Unit = {
-      val stream = mkStream(Seq(-1, -2, -3))
-      assertEquals(255, stream.read())
-      assertEquals(254, stream.read())
-      assertEquals(253, stream.read())
-      assertEquals(-1, stream.read())
-    }
+  @Test def should_return_positive_integers_when_calling_read(): Unit = {
+    val stream = mkStream(Seq(-1, -2, -3))
+    assertEquals(255, stream.read())
+    assertEquals(254, stream.read())
+    assertEquals(253, stream.read())
+    assertEquals(-1, stream.read())
   }
 }
