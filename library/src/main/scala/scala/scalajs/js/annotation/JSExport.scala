4a672fe4888294dbee22e53d32a4ec2b4fb27a3b
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-07-10 14:59:15 +0200

    Fix #2513: Annotate `@JSExport` with `@field @getter @setter`.
    In addition, the compiler and the `js.use(x).as[T]` macro need to
    look for `@JSExport` on the getter/setter itself, if there is no
    accessed field. In theory, it should do so unconditionally, but
    backward binary compatibility demand that we still recognize
    exports built with a previous version of the library.
    
    Incidentally, this provides a better fix to the old issue #970.
    It is not necessary to use `@(JSExport @meta.field)` for
    constructor parameter fields anymore.

--- library/src/main/scala/scala/scalajs/js/annotation/JSExport.scala
+++ library/src/main/scala/scala/scalajs/js/annotation/JSExport.scala
@@ -10,10 +10,13 @@
 
 package scala.scalajs.js.annotation
 
+import scala.annotation.meta._
+
 /** Specifies that the given entity should be exported for use in raw JS.
  *
  *  @see [[http://www.scala-js.org/doc/export-to-javascript.html Export Scala.js APIs to JavaScript]]
  */
+@field @getter @setter
 class JSExport extends scala.annotation.StaticAnnotation {
   def this(name: String) = this()
 }
