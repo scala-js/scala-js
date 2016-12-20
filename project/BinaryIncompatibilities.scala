7251bb8abd9a83fb8094ad3c0b1002f808cb50a2
Sébastien Doeraene <sjrdoeraene@gmail.com>
2016-12-20 22:45:48 +0100

    Towards 0.6.15.

--- project/BinaryIncompatibilities.scala
+++ project/BinaryIncompatibilities.scala
@@ -3,45 +3,9 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object BinaryIncompatibilities {
   val IR = Seq(
-      // private, not an issue
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.ir.Infos#GenInfoTraverser.generateExportedConstructorsInfo")
   )
 
   val Tools = Seq(
-      // private[emitter], not an issue
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.ScalaJSClassEmitter.genDeclareModule"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.ScalaJSClassEmitter.genDeclareTypeData"),
-
-      // private, not an issue
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring#Env.this"),
-      ProblemFilters.exclude[MissingMethodProblem](
-          "org.scalajs.core.tools.linker.backend.emitter.JSDesugaring#JSDesugar.this"),
-
-      // Breaking (remove js.EmptyTree)
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#Let.apply"),
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#Let.copy"),
-      ProblemFilters.exclude[IncompatibleResultTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#Let.rhs"),
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#Let.this"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.core.tools.javascript.Trees$Try"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.core.tools.javascript.Trees$Try$"),
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#VarDef.apply"),
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#VarDef.copy"),
-      ProblemFilters.exclude[IncompatibleResultTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#VarDef.rhs"),
-      ProblemFilters.exclude[IncompatibleMethTypeProblem](
-          "org.scalajs.core.tools.javascript.Trees#VarDef.this")
   )
 
   val JSEnvs = Seq(
--- project/Build.scala
+++ project/Build.scala
@@ -57,7 +57,7 @@ object Build {
   val shouldPartest = settingKey[Boolean](
     "Whether we should partest the current scala version (and fail if we can't)")
 
-  val previousVersion = "0.6.13"
+  val previousVersion = "0.6.14"
   val previousSJSBinaryVersion =
     ScalaJSCrossVersion.binaryScalaJSVersion(previousVersion)
   val previousBinaryCrossVersion =
