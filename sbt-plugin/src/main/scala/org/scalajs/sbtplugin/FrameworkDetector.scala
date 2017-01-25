scala-js/scala-js

commit f46050f245bed6c318e2f2855a811ea6afb5d0cd

Prepare the testing interface to transition to reflective instantiation.
This commit makes all the necessary changes to the testing interface
so that it will be able to cope with reflective instantiation-based
test loading. This means using `@EnableReflectiveInstantiation`
instead of `@JSExportDescendantClasses`/`Objects` on both
`sbt.testing.Framework` and test classes.

This commit does *not*, however, already change the annotations.
This ensures that the changes are "binary-compatible", in the sense
that a new sbt plugin and (possibly) a new testing interface are
able to run testing frameworks designed with the old export-based
system.

sbt-plugin/src/main/scala/org/scalajs/sbtplugin/FrameworkDetector.scala
@@ -40,31 +40,10 @@ private[sbtplugin] final class FrameworkDetector(jsEnv: JSEnv,
         "use strict";
 
         var data = ${jsonToString(data)};
-
-        function frameworkExists(name) {
-          var parts = name.split(".");
-          var obj = exportsNamespace;
-          for (var i = 0; i < parts.length; ++i) {
-            obj = obj[parts[i]];
-            if (obj === void 0)
-              return false;
-          }
-          return true;
-        }
-
-        for (var i = 0; i < data.length; ++i) {
-          var gotOne = false;
-          for (var j = 0; j < data[i].length; ++j) {
-            if (frameworkExists(data[i][j])) {
-              console.log("$ConsoleFrameworkPrefix" + data[i][j]);
-              gotOne = true;
-              break;
-            }
-          }
-          if (!gotOne) {
-            // print an empty line with prefix to zip afterwards
-            console.log("$ConsoleFrameworkPrefix");
-          }
+        var results =
+          exportsNamespace.org.scalajs.testinterface.internal.detectFrameworks(data);
+        for (var i = 0; i < results.length; ++i) {
+          console.log("$ConsoleFrameworkPrefix" + (results[i] || ""));
         }
       })($exportsNamespaceExpr);
     """

test-interface/src/main/scala/org/scalajs/testinterface/internal/FrameworkDetector.scala
@@ -1,0 +1,39 @@
+package org.scalajs.testinterface.internal
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+import scala.scalajs.js.JSConverters._
+import scala.scalajs.reflect.Reflect
+
+private[internal] object FrameworkDetector {
+  @JSExportTopLevel("org.scalajs.testinterface.internal.detectFrameworks")
+  def detectFrameworks(
+      frameworksData: js.Array[js.Array[String]]): js.Array[js.UndefOr[String]] = {
+
+    def frameworkExistsInReflect(name: String): Boolean = {
+      Reflect.lookupInstantiatableClass(name).exists { clazz =>
+        classOf[sbt.testing.Framework].isAssignableFrom(clazz.runtimeClass)
+      }
+    }
+
+    def frameworkExistsInExportsNamespace(name: String): Boolean = {
+      /* This happens for testing frameworks developed before 0.6.15 that have
+       * not yet updated to using reflective instantiation, and are still
+       * using exports.
+       * Note that here, we have to assume that whatever we find is indeed a
+       * proper class export for a class extending sbt.testing.Framework.
+       */
+      val exportsNamespace =
+        scala.scalajs.runtime.environmentInfo.exportsNamespace
+      name.split('.').foldLeft[js.UndefOr[js.Dynamic]](exportsNamespace) {
+        (prev, part) => prev.map(_.selectDynamic(part))
+      }.isDefined
+    }
+
+    def frameworkExists(name: String): Boolean =
+      frameworkExistsInReflect(name) || frameworkExistsInExportsNamespace(name)
+
+    for (frameworkNames <- frameworksData)
+      yield frameworkNames.find(frameworkExists(_)).orUndefined
+  }
+}

sbt-plugin-test/testFramework/src/main/scala/sbttest/framework/DummyTask.scala
@@ -15,7 +15,7 @@ final class DummyTask(
     try {
       // Just create a new instance.
       val inst = TestUtils.newInstance(taskDef.fullyQualifiedName,
-        runner.testClassLoader)(Seq())
+          runner.testClassLoader, Seq())(Seq())
 
       eventHandler.handle(new DummyEvent(taskDef, None))
       loggers.foreach(_.info(s"Success: ${taskDef.fullyQualifiedName}"))

stubs/src/main/scala/org/scalajs/testinterface/TestUtils.scala
@@ -15,8 +15,15 @@ object TestUtils {
   import scala.reflect.macros._ // shadows blackbox from above
   import blackbox.Context
 
+  @deprecated(
+      "Use the overload with explicit formal constructor parameter types.",
+      "0.6.15")
   def newInstance(name: String, loader: ClassLoader)(args: Seq[AnyRef]): AnyRef =
     macro newInstance_impl
+
+  def newInstance(name: String, loader: ClassLoader, paramTypes: Seq[Class[_]])(
+      args: Seq[AnyRef]): AnyRef =
+    macro newInstance_impl2
 
   def newInstance_impl(c: Context)(name: c.Expr[String],
       loader: c.Expr[ClassLoader])(
@@ -31,6 +38,15 @@ object TestUtils {
     }
 
     val ctor = ctors.head
+    ctor.newInstance(args.splice: _*).asInstanceOf[AnyRef]
+  }
+
+  def newInstance_impl2(c: Context)(name: c.Expr[String],
+      loader: c.Expr[ClassLoader], paramTypes: c.Expr[Seq[Class[_]]])(
+      args: c.Expr[Seq[AnyRef]]): c.Expr[AnyRef] = c.universe.reify {
+
+    val clazz = loader.splice.loadClass(name.splice)
+    val ctor = clazz.getConstructor(paramTypes.splice.toArray[Class[_]]: _*)
     ctor.newInstance(args.splice: _*).asInstanceOf[AnyRef]
   }
 

test-interface/src/main/scala/org/scalajs/testinterface/TestDetector.scala
@@ -1,7 +1,8 @@
 package org.scalajs.testinterface
 
 import scala.scalajs.js
-import js.annotation.JSGlobalScope
+import scala.scalajs.js.annotation.JSGlobalScope
+import scala.scalajs.reflect.Reflect
 
 import org.scalajs.testinterface.internal.TaskDefSerializer
 
@@ -26,18 +27,31 @@ private[scalajs] object TestDetector {
   }
 
   private def tryLoadFramework(names: js.Array[String]): Option[Framework] = {
-    def tryLoadName(name: String) = {
-      val parts = name.split('.')
+    def tryLoadFromReflect(name: String): Option[Framework] = {
+      Reflect.lookupInstantiatableClass(name).collect {
+        case clazz if classOf[Framework].isAssignableFrom(clazz.runtimeClass) =>
+          clazz.newInstance().asInstanceOf[Framework]
+      }
+    }
 
-      val ctor = parts.foldLeft[js.UndefOr[js.Dynamic]](js.Dynamic.global)(
-          (parent, name) => parent.map(_.selectDynamic(name)))
+    def tryLoadFromExportsNamespace(name: String): Option[Framework] = {
+      val exportsNamespace =
+        scala.scalajs.runtime.linkingInfo.envInfo.exportsNamespace
+
+      val parts = name.split('.')
+      val ctor = parts.foldLeft[js.UndefOr[js.Dynamic]](exportsNamespace) {
+        (parent, name) => parent.map(_.selectDynamic(name))
+      }
 
       ctor.map(js.Dynamic.newInstance(_)()).collect {
         case framework: Framework => framework
       }.toOption
     }
 
-    names.toStream.map(tryLoadName).flatten.headOption
+    def tryLoad(name: String): Option[Framework] =
+      tryLoadFromReflect(name).orElse(tryLoadFromExportsNamespace(name))
+
+    names.toStream.map(tryLoad).flatten.headOption
   }
 
   // Copied from sbt.TestFramework

test-interface/src/main/scala/org/scalajs/testinterface/TestUtils.scala
@@ -1,24 +1,94 @@
 package org.scalajs.testinterface
 
 import scala.scalajs.js
+import scala.scalajs.reflect._
 
 object TestUtils {
 
-  def newInstance(name: String, loader: ClassLoader)(args: Seq[AnyRef]): AnyRef = {
-    val ctor = deepSelect(namespace(loader), name)
-    js.Dynamic.newInstance(ctor)(args.asInstanceOf[Seq[js.Any]]: _*)
+  /** Instantiates the class given by its fully qualified name.
+   *
+   *  The target class must be exported under its fully qualified name.
+   *
+   *  This overload of `newInstance` cannot instantiate classes with an
+   *  ancestor annotated with
+   *  [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]].
+   *
+   *  Prefer using the other overload of `newInstance` for new code, which
+   *  supports reflective instantiation in addition to exports-based
+   *  instantiation.
+   */
+  @deprecated(
+      "Use the overload with explicit formal constructor parameter types.",
+      "0.6.15")
+  def newInstance(name: String, loader: ClassLoader)(
+      args: Seq[AnyRef]): AnyRef = {
+    Reflect.lookupInstantiatableClass(name).fold[AnyRef] {
+      val ctor = deepSelect(namespace(loader), name)
+      js.Dynamic.newInstance(ctor)(args.asInstanceOf[Seq[js.Any]]: _*)
+    } { clazz =>
+      throw new InstantiationException(
+          s"The class '$name' should be loaded through reflective " +
+          "instantiation, but the overload of TestUtils.newIntance() that " +
+          "was used does not support it. You can fix it by calling the other " +
+          "overload of TestUtils.newInstance().")
+    }
   }
 
+  /** Instantiates the class given by its fully qualified name.
+   *
+   *  The target class must either
+   *
+   *  - extend a class or trait annotated with
+   *    [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]], or
+   *  - be exported under its fully qualified name.
+   *
+   *  In the former case, the overload is selected based on `paramTypes`. In
+   *  the latter case, the overload is selected by the usual export overload
+   *  resolution mechanism.
+   */
+  def newInstance(name: String, loader: ClassLoader, paramTypes: Seq[Class[_]])(
+      args: Seq[Any]): AnyRef = {
+    require(args.size == paramTypes.size, "argument count mismatch")
+
+    Reflect.lookupInstantiatableClass(name).fold[AnyRef] {
+      val ctor = deepSelect(namespace(loader), name)
+      js.Dynamic.newInstance(ctor)(args.asInstanceOf[Seq[js.Any]]: _*)
+    } { clazz =>
+      val ctor = clazz.declaredConstructors.find {
+        _.parameterTypes == paramTypes
+      }.getOrElse {
+        throw new InstantiationError(name)
+      }
+      ctor.newInstance(args: _*).asInstanceOf[AnyRef]
+    }
+  }
+
+  /** Loads the module given by its fully qualified name.
+   *
+   *  The target object must either
+   *
+   *  - extend a class or trait annotated with
+   *    [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]], or
+   *  - be exported under its fully qualified name.
+   *
+   *  The name *must not* include the trailing `$` that is part of the module
+   *  name, as added by the Scala compiler.
+   */
   def loadModule(name: String, loader: ClassLoader): AnyRef = {
-    val accessor = deepSelect(namespace(loader), name)
-    accessor()
+    Reflect.lookupLoadableModuleClass(name + "$").fold[AnyRef] {
+      val accessor = deepSelect(namespace(loader), name)
+      accessor()
+    } { loadableModule =>
+      loadableModule.loadModule().asInstanceOf[AnyRef]
+    }
   }
 
   private def namespace(loader: ClassLoader): js.Dynamic = {
     loader match {
-      case loader: ScalaJSClassLoader => loader.namespace
-      case _ => throw new IllegalArgumentException(
-          "Need a ScalaJSClassLoader.")
+      case loader: ScalaJSClassLoader =>
+        loader.namespace
+      case _ =>
+        throw new IllegalArgumentException("Need a ScalaJSClassLoader.")
     }
   }
 

test-interface/src/main/scala/org/scalajs/testinterface/internal/FrameworkLoader.scala
@@ -1,17 +1,22 @@
 package org.scalajs.testinterface.internal
 
 import scala.scalajs.js
+import scala.scalajs.reflect.Reflect
 
 import sbt.testing.Framework
 
 private[internal] object FrameworkLoader {
 
   def loadFramework(frameworkName: String): Framework = {
-    val exportsNamespace =
-      scala.scalajs.runtime.environmentInfo.exportsNamespace
-    val parts = frameworkName.split('.')
-    val ctor = parts.foldLeft(exportsNamespace)(_.selectDynamic(_))
-    js.Dynamic.newInstance(ctor)().asInstanceOf[Framework]
+    Reflect.lookupInstantiatableClass(frameworkName).fold[Framework] {
+      val exportsNamespace =
+        scala.scalajs.runtime.environmentInfo.exportsNamespace
+      val parts = frameworkName.split('.')
+      val ctor = parts.foldLeft(exportsNamespace)(_.selectDynamic(_))
+      js.Dynamic.newInstance(ctor)().asInstanceOf[Framework]
+    } { clazz =>
+      clazz.newInstance().asInstanceOf[Framework]
+    }
   }
 
 }

test-suite/js/src/test/scala/org/scalajs/testsuite/junit/JUnitUtil.scala
@@ -3,17 +3,17 @@ package org.scalajs.testsuite.junit
 import org.scalajs.junit.JUnitTestBootstrapper
 import org.junit.Assert.fail
 
+import org.scalajs.testinterface._
+
 object JUnitUtil {
   private final val BootstrapperSuffix = "$scalajs$junit$bootstrapper"
 
   def loadBootstrapper(classFullName: String): JUnitTestBootstrapper = {
     val fullName = s"$classFullName$BootstrapperSuffix"
     try {
-      val exportsNamespace =
-        scala.scalajs.runtime.environmentInfo.exportsNamespace
-      fullName.split('.').foldLeft(exportsNamespace) { (obj, n) =>
-        obj.selectDynamic(n)
-      }.apply().asInstanceOf[JUnitTestBootstrapper]
+      val loader = new ScalaJSClassLoader(
+          scala.scalajs.runtime.environmentInfo.exportsNamespace)
+      TestUtils.loadModule(fullName, loader).asInstanceOf[JUnitTestBootstrapper]
     } catch {
       case ex: Throwable =>
         throw new AssertionError(s"could not load $fullName: ${ex.getMessage}")

commit 4fba8af0e58c9e504f9401046dbce8d2c08f3082

Fix #2752: Survive in `test` if there is no testing framework.
The `test` task should trivially succeed if there is no testing
framework on the classpath. However,
f46050f245bed6c318e2f2855a811ea6afb5d0cd broke this scenario, by
requiring an export from the testing interface to be present.

This commit fixes the scenario to tolerate the absence of that
export. In that case, no candidate testing framework is reported
to be available.

We add `library/test` to the CI matrix as a test for this scenario,
as the `library` project does not and will not contain any tests.

ci/matrix.xml
@@ -57,6 +57,7 @@
     sbtretry 'set inScope(ThisScope in testingExample)(jsEnv := new org.scalajs.jsenv.RetryingComJSEnv(PhantomJSEnv().value))' \
         'set scalaJSOptimizerOptions in testingExample ~= (_.withDisableOptimizer(true))' \
         ++$scala testingExample/test:run testingExample/test &&
+    sbtretry ++$scala library/test &&
     sbtretry ++$scala testSuiteJVM/test testSuiteJVM/clean &&
     sbtretry ++$scala 'testSuite/test:runMain org.scalajs.testsuite.junit.JUnitBootstrapTest' &&
     sbtretry ++$scala testSuite/test &&

sbt-plugin/src/main/scala/org/scalajs/sbtplugin/FrameworkDetector.scala
@@ -39,9 +39,25 @@ private[sbtplugin] final class FrameworkDetector(jsEnv: JSEnv,
       (function(exportsNamespace) {
         "use strict";
 
+        /* #2752: if there is no testing framework at all on the classpath,
+         * the testing interface will not be there, and therefore the
+         * `detectFrameworks` function will not exist. We must therefore be
+         * careful when selecting it.
+         */
+        var namespace = exportsNamespace;
+        namespace = namespace.org || {};
+        namespace = namespace.scalajs || {};
+        namespace = namespace.testinterface || {};
+        namespace = namespace.internal || {};
+        var detectFrameworksFun = namespace.detectFrameworks || (function(data) {
+          var results = [];
+          for (var i = 0; i < data.length; ++i)
+            results.push(void 0);
+          return results;
+        });
+
         var data = ${jsonToString(data)};
-        var results =
-          exportsNamespace.org.scalajs.testinterface.internal.detectFrameworks(data);
+        var results = detectFrameworksFun(data);
         for (var i = 0; i < results.length; ++i) {
           console.log("$ConsoleFrameworkPrefix" + (results[i] || ""));
         }