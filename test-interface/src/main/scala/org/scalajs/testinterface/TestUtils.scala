ba06a2be4aed14d7d453f620235f33d336eb70e4
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-05-05 15:52:27 +0200

    Remove support for loading from the exports namespace in TestUtils.
    Since `@JSExportDescendent{Classes,Objects}` have been removed,
    this support is completely useless.

--- test-interface/src/main/scala/org/scalajs/testinterface/TestUtils.scala
+++ test-interface/src/main/scala/org/scalajs/testinterface/TestUtils.scala
@@ -7,92 +7,40 @@ object TestUtils {
 
   /** Instantiates the class given by its fully qualified name.
    *
-   *  The target class must be exported under its fully qualified name.
-   *
-   *  This overload of `newInstance` cannot instantiate classes with an
-   *  ancestor annotated with
+   *  The target class must extend a class or trait annotated with
    *  [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]].
    *
-   *  Prefer using the other overload of `newInstance` for new code, which
-   *  supports reflective instantiation in addition to exports-based
-   *  instantiation.
-   */
-  @deprecated(
-      "Use the overload with explicit formal constructor parameter types.",
-      "0.6.15")
-  def newInstance(name: String, loader: ClassLoader)(
-      args: Seq[AnyRef]): AnyRef = {
-    Reflect.lookupInstantiatableClass(name).fold[AnyRef] {
-      val ctor = deepSelect(namespace(loader), name)
-      js.Dynamic.newInstance(ctor)(args.asInstanceOf[Seq[js.Any]]: _*)
-    } { clazz =>
-      throw new InstantiationException(
-          s"The class '$name' should be loaded through reflective " +
-          "instantiation, but the overload of TestUtils.newIntance() that " +
-          "was used does not support it. You can fix it by calling the other " +
-          "overload of TestUtils.newInstance().")
-    }
-  }
-
-  /** Instantiates the class given by its fully qualified name.
-   *
-   *  The target class must either
-   *
-   *  - extend a class or trait annotated with
-   *    [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]], or
-   *  - be exported under its fully qualified name.
-   *
-   *  In the former case, the overload is selected based on `paramTypes`. In
-   *  the latter case, the overload is selected by the usual export overload
-   *  resolution mechanism.
+   *  The `paramTypes` argument is used to select the appropriate overloaded
+   *  constructor.
    */
   def newInstance(name: String, loader: ClassLoader, paramTypes: Seq[Class[_]])(
       args: Seq[Any]): AnyRef = {
     require(args.size == paramTypes.size, "argument count mismatch")
 
-    Reflect.lookupInstantiatableClass(name).fold[AnyRef] {
-      val ctor = deepSelect(namespace(loader), name)
-      js.Dynamic.newInstance(ctor)(args.asInstanceOf[Seq[js.Any]]: _*)
-    } { clazz =>
-      val ctor = clazz.declaredConstructors.find {
-        _.parameterTypes == paramTypes
-      }.getOrElse {
-        throw new InstantiationError(name)
-      }
-      ctor.newInstance(args: _*).asInstanceOf[AnyRef]
+    val clazz = Reflect.lookupInstantiatableClass(name).getOrElse {
+      throw new InstantiationError(name)
     }
+    val ctor = clazz.declaredConstructors.find {
+      _.parameterTypes == paramTypes
+    }.getOrElse {
+      throw new InstantiationError(name)
+    }
+    ctor.newInstance(args: _*).asInstanceOf[AnyRef]
   }
 
   /** Loads the module given by its fully qualified name.
    *
-   *  The target object must either
-   *
-   *  - extend a class or trait annotated with
-   *    [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]], or
-   *  - be exported under its fully qualified name.
+   *  The target object must extend a class or trait annotated with
+   *  [[scala.scalajs.reflect.annotation.EnableReflectiveInstantiation]].
    *
    *  The name *must not* include the trailing `$` that is part of the module
    *  name, as added by the Scala compiler.
    */
   def loadModule(name: String, loader: ClassLoader): AnyRef = {
-    Reflect.lookupLoadableModuleClass(name + "$").fold[AnyRef] {
-      val accessor = deepSelect(namespace(loader), name)
-      accessor()
-    } { loadableModule =>
-      loadableModule.loadModule().asInstanceOf[AnyRef]
+    val loadableModule = Reflect.lookupLoadableModuleClass(name + "$").getOrElse {
+      throw new InstantiationError(name)
     }
+    loadableModule.loadModule().asInstanceOf[AnyRef]
   }
-
-  private def namespace(loader: ClassLoader): js.Dynamic = {
-    loader match {
-      case loader: ScalaJSClassLoader =>
-        loader.namespace
-      case _ =>
-        throw new IllegalArgumentException("Need a ScalaJSClassLoader.")
-    }
-  }
-
-  private def deepSelect(receiver: js.Dynamic, name: String) =
-    name.split('.').foldLeft(receiver)((obj, n) => obj.selectDynamic(n))
 
 }
