b54a26c1c2126bedb5b6dd8a450a5e6891e06612
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-06-27 15:40:28 +0200

    Towards 0.6.19.

--- project/Build.scala
+++ project/Build.scala
@@ -56,7 +56,7 @@ object Build {
   val shouldPartest = settingKey[Boolean](
     "Whether we should partest the current scala version (and fail if we can't)")
 
-  val previousVersion = "0.6.17"
+  val previousVersion = "0.6.18"
   val previousSJSBinaryVersion =
     ScalaJSCrossVersion.binaryScalaJSVersion(previousVersion)
   val previousBinaryCrossVersion =
