9371fa909df68e9cf6d0b35ea4085845021b587e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-09-01 00:56:07 +0200

    Towards 0.6.21.

--- project/BinaryIncompatibilities.scala
+++ project/BinaryIncompatibilities.scala
@@ -18,124 +18,14 @@ object BinaryIncompatibilities {
   )
 
   val TestAdapter = Seq(
-      // private[scalajs], not an issue.
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskDefSerializers"),
-
-      // private[testadapter], not an issue.
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.ComUtils"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.ComUtils$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.EventSerializers"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.EventSerializers$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.EventSerializers$DeserializedEvent"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.EventSerializers$EventDeserializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FingerprintSerializers"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FingerprintSerializers$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FingerprintSerializers$DeserializedAnnotatedFingerprint"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FingerprintSerializers$DeserializedSubclassFingerprint"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FingerprintSerializers$FingerprintDeserializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FingerprintSerializers$FingerprintSerializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FrameworkInfo"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FrameworkInfo$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.FrameworkInfo$Deserializer$"),
-      ProblemFilters.exclude[DirectMissingMethodProblem](
-          "org.scalajs.testadapter.ScalaJSRunner.msgHandler"),
-      ProblemFilters.exclude[IncompatibleResultTypeProblem](
-          "org.scalajs.testadapter.ScalaJSRunner.getSlave"),
-      ProblemFilters.exclude[DirectMissingMethodProblem](
-          "org.scalajs.testadapter.ScalaJSTask.fromInfo"),
-      ProblemFilters.exclude[DirectMissingMethodProblem](
-          "org.scalajs.testadapter.ScalaJSTask.this"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.SelectorSerializers"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.SelectorSerializers$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.SelectorSerializers$SelectorDeserializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.SelectorSerializers$SelectorSerializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskDefSerializers$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskDefSerializers$TaskDefDeserializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskDefSerializers$TaskDefSerializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskInfo"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskInfo$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testadapter.TaskInfo$Deserializer$")
   )
 
   val CLI = Seq(
   )
 
   val Library = Seq(
-      /* RuntimeLong lost its `with java.io.Serializable`, but that is not a
-       * problem because it also extends `java.lang.Number`, which itself
-       * implements `java.io.Serializable` anyway.
-       */
-      ProblemFilters.exclude[MissingTypesProblem](
-          "scala.scalajs.runtime.RuntimeLong")
   )
 
   val TestInterface = Seq(
-      // protected[testinterface], not an issue.
-      ProblemFilters.exclude[MissingTypesProblem](
-          "org.scalajs.testinterface.HTMLRunner$"),
-
-      // internal, not an issue.
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.BridgeBase"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.EventSerializer"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.EventSerializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.FingerprintSerializer"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.FingerprintSerializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.FingerprintSerializer$DeserializedAnnotatedFingerprint"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.FingerprintSerializer$DeserializedSubclassFingerprint"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.SelectorSerializer"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.SelectorSerializer$"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.TaskDefSerializer"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.TaskDefSerializer$"),
-      ProblemFilters.exclude[IncompatibleResultTypeProblem](
-          "org.scalajs.testinterface.TestDetector#RawDefinitions.definedTests"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.ThrowableSerializer"),
-      ProblemFilters.exclude[MissingClassProblem](
-          "org.scalajs.testinterface.internal.ThrowableSerializer$"),
-      ProblemFilters.exclude[MissingTypesProblem](
-          "org.scalajs.testinterface.internal.Master"),
-      ProblemFilters.exclude[DirectMissingMethodProblem](
-          "org.scalajs.testinterface.internal.Master.handleMsgImpl"),
-      ProblemFilters.exclude[MissingTypesProblem](
-          "org.scalajs.testinterface.internal.Slave"),
-      ProblemFilters.exclude[DirectMissingMethodProblem](
-          "org.scalajs.testinterface.internal.Slave.handleMsgImpl")
   )
 }
--- project/Build.scala
+++ project/Build.scala
@@ -56,7 +56,7 @@ object Build {
   val shouldPartest = settingKey[Boolean](
     "Whether we should partest the current scala version (and fail if we can't)")
 
-  val previousVersion = "0.6.19"
+  val previousVersion = "0.6.20"
   val previousSJSBinaryVersion =
     ScalaJSCrossVersion.binaryScalaJSVersion(previousVersion)
   val previousBinaryCrossVersion =
