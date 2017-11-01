b481f83a3cc38cac0da8de706469ed8f15bf0859
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-11-01 11:10:55 +0100

    Remove the `crossScalaVersions` setting from the build.
    Due to the high variance about which projects build with what
    versions, we can never reasonably use the `+` command of sbt.
    Therefore, this setting is not actually used, and is only something
    we need to update when upgrading versions, for no benefit.

--- project/Build.scala
+++ project/Build.scala
@@ -428,28 +428,6 @@ object Build {
   val thisBuildSettings = (
       inScope(Global)(ScalaJSPlugin.globalSettings)
   ) ++ Seq(
-      // Most of the projects cross-compile
-      crossScalaVersions := Seq(
-        "2.10.2",
-        "2.10.3",
-        "2.10.4",
-        "2.10.5",
-        "2.10.6",
-        "2.11.0",
-        "2.11.1",
-        "2.11.2",
-        "2.11.4",
-        "2.11.5",
-        "2.11.6",
-        "2.11.7",
-        "2.11.8",
-        "2.11.11",
-        "2.12.0",
-        "2.12.1",
-        "2.12.2",
-        "2.12.3",
-        "2.13.0-M2"
-      ),
       // JDK version we are running with
       javaVersion in Global := {
         val fullVersion = System.getProperty("java.version")
