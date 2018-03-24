a6e5e41b5c19d299fa6d499aeb0b3410a54ff469
Tobias Schlatter <tobias@meisch.ch>
2018-03-25 00:07:41 +0100

    Move NodeVirtualJarFile (and related) to main packages
    We make them private[scalajs] for now. We'll follow up in #3330 to
    either make them all private (in a separate package) or all public.

--- io/js/src/main/scala/org/scalajs/io/NodeVirtualFiles.scala
+++ io/js/src/main/scala/org/scalajs/io/NodeVirtualFiles.scala
@@ -52,3 +52,62 @@ private[io] object NodeFS extends js.Object {
   def readFileSync(path: String, enc: Enc): String = js.native
   def statSync(path: String): Stat = js.native
 }
+
+private[scalajs] class NodeVirtualJarFile(file: String)
+    extends NodeVirtualBinaryFile(file) with VirtualFileContainer {
+
+  import NodeVirtualJarFile._
+
+  def listEntries[T](p: String => Boolean)(
+      makeResult: (String, InputStream) => T): List[T] = {
+    import js.Dynamic.{global => g}
+
+    val stream = inputStream
+    try {
+      /* Build a Uint8Array with the content of this jar file.
+       * We know that in practice, NodeVirtualBinaryFile#inputStream returns
+       * an ArrayBufferInputStream, so we just fetch its internal ArrayBuffer
+       * rather than copying.
+       *
+       * Since we have NodeVirtualBinaryFile under our control, in the same
+       * repository, we can make this assumption. Should we change
+       * NodeVirtualBinaryFile, this test will immediately fail, and we can
+       * adapt it.
+       */
+      val data = stream match {
+        case stream: ArrayBufferInputStream =>
+          // Simulate reading all the data
+          while (stream.skip(stream.available()) > 0) {}
+          new Uint8Array(stream.buffer, stream.offset, stream.length)
+        case _ =>
+          throw new AssertionError(
+              s"Uh! '$file' was not read as an ArrayBufferInputStream")
+      }
+
+      val zip = new JSZip(data)
+
+      for ((name, entry) <- zip.files.toList if p(name)) yield {
+        val entryStream = new ArrayBufferInputStream(entry.asArrayBuffer())
+        try {
+          makeResult(name, entryStream)
+        } finally {
+          entryStream.close()
+        }
+      }
+    } finally {
+      stream.close()
+    }
+  }
+}
+
+private object NodeVirtualJarFile {
+  @js.native
+  @JSImport("jszip", JSImport.Default)
+  private class JSZip(data: Uint8Array) extends js.Object {
+    def files: js.Dictionary[JSZipEntry] = js.native
+  }
+
+  private trait JSZipEntry extends js.Object {
+    def asArrayBuffer(): ArrayBuffer
+  }
+}
