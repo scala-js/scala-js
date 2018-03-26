891a38c757efa30a609c9691ea6b4ad76309ce56
Tobias Schlatter <tobias@meisch.ch>
2018-03-26 07:04:06 +0200

    Make VirtualJarFile JVM only
    VirtualJarFile was already JVM only in practice (since javalib-ex got
    removed).

--- io/shared/src/main/scala/org/scalajs/io/VirtualFiles.scala
+++ io/shared/src/main/scala/org/scalajs/io/VirtualFiles.scala
@@ -2,7 +2,6 @@ package org.scalajs.io
 
 import java.io._
 import java.net.URI
-import java.util.zip.{ZipInputStream, ZipEntry}
 
 /** A virtual input file.
  */
@@ -137,32 +136,4 @@ trait VirtualFileContainer extends VirtualFile {
    */
   def listEntries[T](p: String => Boolean)(
       makeResult: (String, InputStream) => T): List[T]
-}
-
-/** A virtual jar file. */
-trait VirtualJarFile extends VirtualFileContainer with VirtualBinaryFile {
-  import VirtualJarFile._
-
-  def listEntries[T](p: String => Boolean)(
-      makeResult: (String, InputStream) => T): List[T] = {
-    val stream = new ZipInputStream(inputStream)
-    try {
-      val streamIgnoreClose = new IgnoreCloseFilterInputStream(stream)
-      Iterator.continually(stream.getNextEntry())
-        .takeWhile(_ != null)
-        .filter(entry => p(entry.getName))
-        .map(entry => makeResult(entry.getName, streamIgnoreClose))
-        .toList
-    } finally {
-      stream.close()
-    }
-  }
-}
-
-private object VirtualJarFile {
-  private final class IgnoreCloseFilterInputStream(in: InputStream)
-      extends FilterInputStream(in) {
-
-    override def close(): Unit = ()
-  }
 }
