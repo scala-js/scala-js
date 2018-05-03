f5c9461e1228ba22ae65ba3bb7b5000d896cc597
Tobias Schlatter <tobias@meisch.ch>
2018-05-03 18:40:39 +0200

    Remove unused helper.

--- test-common/src/main/scala/org/scalajs/testcommon/FutureUtil.scala
+++ test-common/src/main/scala/org/scalajs/testcommon/FutureUtil.scala
@@ -2,7 +2,6 @@ package org.scalajs.testcommon
 
 import scala.util._
 import scala.concurrent._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 private[scalajs] object FutureUtil {
   /** Same as Future.fromTry(x) but works in 2.10 */
@@ -10,14 +9,5 @@ private[scalajs] object FutureUtil {
     val promise = Promise[T]
     promise.complete(x)
     promise.future
-  }
-
-  implicit class RichFuture[T](val __self: Future[T]) extends AnyVal {
-    def liftToTry: Future[Try[T]] =
-      __self.map(Success(_)).recover(pf(Failure(_)))
-  }
-
-  private def pf[A, B](f: A => B): PartialFunction[A, B] = {
-    case x => f(x)
   }
 }
