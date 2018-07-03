9e4ae15954a16e70ee9c859838e1db9557b78780
Sébastien Doeraene <sjrdoeraene@gmail.com>
2018-07-03 12:04:05 +0200

    Towards 1.0.0 again.

--- ir/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -10,7 +10,7 @@ object ScalaJSVersions {
    */
 
   /** Scala.js version. */
-  val current: String = "1.0.0-M5"
+  val current: String = "1.0.0-SNAPSHOT"
 
   /** true iff the Scala.js version is a snapshot version. */
   val currentIsSnapshot: Boolean = current endsWith "-SNAPSHOT"
