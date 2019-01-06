21e601d5926a54fe58eb7b380f5e80c831c40d85
Tobias Schlatter <tobias@meisch.ch>
2019-01-06 19:47:32 +0100

    Re-organize JUnit execution code
    * Test state reporting (both logging and events) goes into Reporting
    * Task execution goes into JUnitTask

--- junit-runtime/src/main/scala/org/scalajs/junit/JUnitTask.scala
+++ junit-runtime/src/main/scala/org/scalajs/junit/JUnitTask.scala
@@ -27,38 +27,157 @@ private[junit] final class JUnitTask(val taskDef: TaskDef,
   }
 
   def execute(eventHandler: EventHandler, loggers: Array[Logger]): Array[Task] = {
-    val fullClassName = taskDef.fullyQualifiedName
-    val richLogger = new RichLogger(loggers, runSettings, fullClassName)
+    val reporter = new Reporter(eventHandler, loggers, runSettings, taskDef)
 
-    val bootstrapperName = fullClassName + "$scalajs$junit$bootstrapper"
-
-    def errorWhileLoadingClass(t: Throwable): Unit = {
-      richLogger.log(_.error, "Error while loading test class: " + fullClassName)
-      richLogger.trace(t)
-      val selector = new TestSelector(fullClassName)
-      val optThrowable = new OptionalThrowable(t)
-      val ev = new JUnitEvent(taskDef, Status.Failure, selector, optThrowable)
-      eventHandler.handle(ev)
-    }
-
-    Try {
-      Reflect
-        .lookupLoadableModuleClass(bootstrapperName + "$")
-        .getOrElse(throw new ClassNotFoundException(s"Cannot find $bootstrapperName$$"))
-        .loadModule()
-    } match {
-      case Success(bootstrapper: Bootstrapper) =>
-        new JUnitExecuteTest(taskDef, runSettings, bootstrapper,
-            richLogger, eventHandler).executeTests()
-
-      case Success(_) =>
-        val msg = s"Expected $bootstrapperName to extend Bootstrapper"
-        errorWhileLoadingClass(new Exception(msg))
-
-      case Failure(exception) =>
-        errorWhileLoadingClass(exception)
+    for (bootstrapper <- loadBootstrapper(reporter)) {
+      executeTests(bootstrapper, reporter)
     }
 
     Array()
+  }
+
+  private def executeTests(bootstrapper: Bootstrapper, reporter: Reporter): Unit = {
+    reporter.reportRunStarted()
+
+    var failed = 0
+    var ignored = 0
+    var total = 0
+
+    val (errors, timeInSeconds) = runTestLifecycle {
+      ()
+    } { _ =>
+      bootstrapper.beforeClass()
+
+      for (method <- bootstrapper.tests) {
+        if (method.ignored) {
+          reporter.reportIgnored(Some(method.name))
+          ignored += 1
+        } else {
+          failed += executeTestMethod(bootstrapper, method, reporter)
+          total += 1
+        }
+      }
+    } { _ =>
+      bootstrapper.afterClass()
+    }
+
+    errors match {
+      case e :: Nil if isAssumptionViolation(e) =>
+        reporter.reportIgnored(None)
+        ignored += 1
+
+      case es =>
+        failed += es.size
+        reporter.reportErrors("Test ", None, timeInSeconds, es)
+    }
+
+    reporter.reportRunFinished(failed, ignored, total, timeInSeconds)
+  }
+
+  private[this] def executeTestMethod(bootstrapper: Bootstrapper, test: TestMetadata,
+      reporter: Reporter): Int = {
+    reporter.reportTestStarted(test.name)
+
+    val (errors, timeInSeconds) = runTestLifecycle {
+      bootstrapper.newInstance()
+    } { instance =>
+      bootstrapper.before(instance)
+      handleExpected(test.annotation.expected) {
+        bootstrapper.invokeTest(instance, test.name)
+      }
+    } {
+      bootstrapper.after(_)
+    }
+
+    val failed = errors match {
+      case e :: Nil if isAssumptionViolation(e) =>
+        reporter.reportAssumptionViolation(test.name, timeInSeconds, e)
+        0
+
+      case es =>
+        reporter.reportErrors("Test ", Some(test.name), timeInSeconds, es)
+        es.size
+    }
+
+    reporter.reportTestFinished(test.name, errors.isEmpty, timeInSeconds)
+
+    // Scala.js-specific: timeouts are warnings only, after the fact
+    val timeout = test.annotation.timeout
+    if (timeout != 0 && timeout <= timeInSeconds) {
+      reporter.log(_.warn, "Timeout: took " + timeInSeconds + " sec, expected " +
+          (timeout.toDouble / 1000) + " sec")
+    }
+
+    failed
+  }
+
+  private def loadBootstrapper(reporter: Reporter): Option[Bootstrapper] = {
+    val bootstrapperName =
+      taskDef.fullyQualifiedName + "$scalajs$junit$bootstrapper$"
+
+    try {
+      val b = Reflect
+        .lookupLoadableModuleClass(bootstrapperName)
+        .getOrElse(throw new ClassNotFoundException(s"Cannot find $bootstrapperName"))
+        .loadModule()
+
+      b match {
+        case b: Bootstrapper => Some(b)
+
+        case _ =>
+          throw new ClassCastException(s"Expected $bootstrapperName to extend Bootstrapper")
+      }
+    } catch {
+      case t: Throwable =>
+        reporter.reportErrors("Error while loading test class ", None, 0, List(t))
+        None
+    }
+  }
+
+  private def handleExpected(expectedException: Class[_ <: Throwable])(body: => Unit) = {
+    val wantException = expectedException != classOf[org.junit.Test.None]
+    val succeeded = try {
+      body
+      true
+    } catch {
+      case t if expectedException.isInstance(t) => false
+
+      case t if wantException =>
+        val expName = expectedException.getName
+        val gotName = t.getClass.getName
+        throw new Exception(
+            s"Unexpected exception, expected<$expName> but was<$gotName>", t)
+    }
+
+    if (succeeded && wantException)
+      throw new AssertionError("Expected exception: " + expectedException.getName)
+  }
+
+  private def runTestLifecycle[T](build: => T)(body: T => Unit)(
+      after: T => Unit): (List[Throwable], Double) = {
+    val startTime = System.nanoTime
+
+    var exceptions: List[Throwable] = Nil
+    try {
+      val x = build
+      try {
+        body(x)
+      } catch {
+        case t: Throwable => exceptions ::= t
+      } finally {
+        after(x)
+      }
+    } catch {
+      case t: Throwable => exceptions ::= t
+    }
+
+    val timeInSeconds = (System.nanoTime - startTime).toDouble / 1000000000
+
+    (exceptions.reverse, timeInSeconds)
+  }
+
+  private def isAssumptionViolation(ex: Throwable): Boolean = {
+    ex.isInstanceOf[org.junit.AssumptionViolatedException] ||
+    ex.isInstanceOf[org.junit.internal.AssumptionViolatedException]
   }
 }
