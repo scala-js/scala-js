03faf11c670ed0c8eb973e792a771a91c265c320
Sébastien Doeraene <sjrdoeraene@gmail.com>
2019-01-10 15:04:35 +0100

    [no-master] Backport UndefinedBehaviorError from 1.x for 2.13.x.
    Due to changes in `ControlThrowable` upstream, the old version of
    `UndefinedBehaviorError` does not compile anymore. For new versions
    of Scala, we therefore already use the new version coming from
    Scala.js 1.x, which does not rely on `ControlThrowable`. It
    requires JDK 8, but so does Scala 2.13.x anyway.

---
+++ library/src/main/scala-new-ube/scala/scalajs/runtime/UndefinedBehaviorError.scala
@@ -0,0 +1,33 @@
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.scalajs.runtime
+
+/** Error thrown when an undefined behavior in Fatal mode has been detected.
+ *  This error should never be caught. It indicates a severe programming bug.
+ *  In Unchecked mode, the program may behave arbitrarily.
+ *  The `cause` is set to the exception that would have been thrown if the
+ *  given behavior was in Compliant mode.
+ *  If your program relies on the proper kind of exception being thrown, as if
+ *  running on the JVM, you should set the appropriate behavior to Compliant.
+ *  Note that this will have (potentially major) performance impacts.
+ */
+class UndefinedBehaviorError(message: String, cause: Throwable)
+    extends java.lang.VirtualMachineError(message, cause) {
+
+  def this(message: String) = this(message, null)
+
+  def this(cause: Throwable) =
+    this(if (cause == null) null else cause.toString, cause)
+
+  def this() = this(null, null)
+}
