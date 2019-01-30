7b15244d5add8c6fd9c1896354af8d26b0a0afeb
Tobias Schlatter <tobias@meisch.ch>
2019-01-30 13:15:38 +0100

    Clean-up ScalaConsoleLogger code
    - Use pattern match instead of nested if.
    - Align with coding style.

--- logging/shared/src/main/scala/org/scalajs/logging/ScalaConsoleLogger.scala
+++ logging/shared/src/main/scala/org/scalajs/logging/ScalaConsoleLogger.scala
@@ -13,14 +13,14 @@
 package org.scalajs.logging
 
 class ScalaConsoleLogger(minLevel: Level = Level.Debug) extends Logger {
-  def log(level: Level, message: =>String): Unit = if (level >= minLevel) {
-    if (level == Level.Warn || level == Level.Error)
-      scala.Console.err.println(message)
-    else
-      scala.Console.out.println(message)
+  def log(level: Level, message: => String): Unit = level match {
+    case _ if level < minLevel    =>
+    case Level.Warn | Level.Error => scala.Console.err.println(message)
+    case Level.Info | Level.Debug => scala.Console.out.println(message)
   }
 
-  def trace(t: => Throwable): Unit =
+  def trace(t: => Throwable): Unit = {
     // This is error level, so no checking
     t.printStackTrace()
+  }
 }
