dd2712bd095bfc4289f38242a282a6fd70b0c6c4
exoego <mogami@exoego.net>
2019-09-27 11:32:34 +0900

    Add js.BigInt for ES2019 (ES10)

---
+++ library/src/main/scala/scala/scalajs/js/typedarray/BigInt64Array.scala
@@ -0,0 +1,51 @@
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.scalajs.js.typedarray
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation._
+
+/** <span class="badge badge-ecma2019" style="float: right;">ECMAScript 2019</span>
+ *
+ *  A [[TypedArray]] of signed 64-bit integers represented as [[js.BigInt]].
+ */
+@js.native
+@JSGlobal
+class BigInt64Array private[this] () extends TypedArray[js.BigInt, BigInt64Array] {
+
+  /** Constructs a BigInt64Array with the given length. Initialized to all 0 */
+  def this(length: Int) = this()
+
+  /** Creates a new BigInt64Array with the same elements than the given TypedArray
+   *
+   *  Each elements must be BigInt (no conversion).
+   */
+  def this(typedArray: TypedArray[js.BigInt, _]) = this()
+
+  /** Creates a new BigInt64Array with the elements in the given array.
+   *
+   *  Each elements must be BigInt (no conversion).
+   */
+  def this(array: js.Iterable[js.BigInt]) = this()
+
+  /** Creates a BigInt64Array view on the given ArrayBuffer */
+  def this(buffer: ArrayBuffer, byteOffset: Int = 0, length: Int = js.native) = this()
+
+}
+
+/** <span class="badge badge-ecma2019" style="float: right;">ECMAScript 2019</span>
+ *  [[BigInt64Array]] companion
+ */
+@js.native
+@JSGlobal
+object BigInt64Array extends TypedArrayStatic[js.BigInt, BigInt64Array]
---
+++ test-suite/js/src/test/scala/org/scalajs/testsuite/library/BigIntTest.scala
@@ -0,0 +1,134 @@
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package org.scalajs.testsuite.library
+
+import org.junit.Assert._
+import org.junit.Assume._
+import org.junit.{BeforeClass, Test}
+import org.scalajs.testsuite.utils.Platform
+
+import scala.scalajs.js
+
+object BigIntTest {
+  @BeforeClass
+  def assumeRuntimeSupportsBigInt(): Unit = {
+    assumeTrue(Platform.executingInNodeJS)
+    val bigIntConstructor =
+      js.Dynamic.global.BigInt.asInstanceOf[js.UndefOr[js.Any]]
+    assumeTrue(bigIntConstructor.isDefined)
+  }
+}
+
+class BigIntTest {
+
+  @Test def apply(): Unit = {
+    val fromString = js.BigInt("9007199254740992")
+    assertEquals(fromString.toString(), "9007199254740992")
+
+    val fromInt = js.BigInt(2147483647)
+    assertEquals(fromInt.toString(), "2147483647")
+
+    val fromDouble = js.BigInt(4294967295d)
+    assertEquals(fromDouble.toString(), "4294967295")
+  }
+
+  @Test def asIntN(): Unit = {
+    val x = js.BigInt.asIntN(8, js.BigInt("256"))
+    assertEquals(x, js.BigInt("0"))
+  }
+
+  @Test def asUintN(): Unit = {
+    val x = js.BigInt(-123)
+    assertEquals(js.BigInt.asUintN(50, x), js.BigInt("1125899906842501"))
+  }
+
+  @Test def toLocaleString(): Unit = {
+    assumeTrue(Platform.executingInNodeJS)
+
+    val bi = js.BigInt("42123456789123456789")
+    assertEquals(bi.toString(), "42123456789123456789")
+
+    val currency = bi
+      .toLocaleString("de-DE", new js.BigInt.ToLocaleStringOptions {
+        style = "currency"
+        currency = "EUR"
+      })
+    // Internationalization support is limited by default in Node.js.
+    // https://nodejs.org/api/intl.html#intl_internationalization_support
+    // toLocaleString may return as same as by toString
+    assertTrue(
+        currency == "€ 42,123,456,789,123,456,789.00" ||
+        currency == "42123456789123456789")
+  }
+
+  @Test def valueOf(): Unit = {
+    val bi = js.BigInt("42123456789123456789")
+    assertEquals(bi, bi.valueOf())
+  }
+
+  @Test def operators(): Unit = {
+    val previousMaxSafe = js.BigInt("9007199254740991")
+    assertEquals(previousMaxSafe, js.BigInt("9007199254740991"))
+
+    val maxPlusOne = previousMaxSafe + js.BigInt(1)
+    assertEquals(maxPlusOne, js.BigInt("9007199254740992"))
+
+    val theFuture = previousMaxSafe + js.BigInt(2)
+    assertEquals(theFuture, js.BigInt("9007199254740993"))
+
+    val multi = previousMaxSafe * js.BigInt(2)
+    assertEquals(multi, js.BigInt("18014398509481982"))
+
+    val subtr = multi - js.BigInt(10)
+    assertEquals(subtr, js.BigInt("18014398509481972"))
+
+    val mod = multi % js.BigInt(10)
+    assertEquals(mod, js.BigInt("2"))
+
+    // TODO: Scala.js does not recongnize ** as operator
+    //    val bigN = js.BigInt(2) ** js.BigInt(54)
+    //    assertEquals(bigN, js.BigInt("18014398509481984"))
+
+    val negative = -js.BigInt("18014398509481984")
+    assertEquals(negative, js.BigInt("-18014398509481984"))
+
+    val bitAnd = js.BigInt(123) & js.BigInt(31)
+    assertEquals(bitAnd, js.BigInt("27"))
+
+    val bitOr = js.BigInt(123) | js.BigInt(31)
+    assertEquals(bitOr, js.BigInt("127"))
+
+    val bitXor = js.BigInt(123) ^ js.BigInt(31)
+    assertEquals(bitXor, js.BigInt("100"))
+
+    val bitLeftShift = js.BigInt(123) << js.BigInt(31)
+    assertEquals(bitLeftShift, js.BigInt("264140488704"))
+
+    val bitRightShift = js.BigInt(12345678) >> js.BigInt(9)
+    assertEquals(bitRightShift, js.BigInt("24112"))
+
+    val bitNot = ~js.BigInt("42")
+    assertEquals(bitNot, js.BigInt("-43"))
+  }
+
+  @Test def compare_with_bigint(): Unit = {
+    val n = js.BigInt("42")
+    assertTrue(n == js.BigInt("42"))
+    assertTrue(n != js.BigInt("43"))
+    assertTrue(n > js.BigInt("41"))
+    assertTrue(n >= js.BigInt("41"))
+    assertTrue(n < js.BigInt("43"))
+    assertTrue(n <= js.BigInt("43"))
+  }
+
+}
