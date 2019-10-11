37867839379eabfe5235e477642a94208058d07c
Nikita Eshkeev <kastolom@gmail.com>
2019-10-11 13:27:10 +0300

    Drop parenthesis from nullary method overrides
    This patch drops parenthesis from the `def factory` methods, since the
    corresponding methods from the parent traits do not have parenthesis.
    
    Dotty does not implicitly inserts parenthesis to nullary methods that so
    if a nullary method overrides another nullary method then they both need
    to either have parenthesis or not. Otherwise Dotty fails to compile such
    methods.
    
    Signed-off-by: Nikita Eshkeev <kastolom@gmail.com>

--- test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/util/AbstractMapTest.scala
+++ test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/util/AbstractMapTest.scala
@@ -17,7 +17,7 @@ import java.{util => ju}
 import scala.reflect.ClassTag
 
 abstract class AbstractMapTest extends MapTest {
-  def factory(): AbstractMapFactory
+  def factory: AbstractMapFactory
 }
 
 abstract class AbstractMapFactory extends MapFactory {
