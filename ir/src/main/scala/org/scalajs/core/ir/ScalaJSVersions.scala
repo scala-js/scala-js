62818633ca8e8c6f3ec612097c07705fbfe970c9
Sébastien Doeraene <sjrdoeraene@gmail.com>
2019-11-20 10:05:02 +0100

    Fix #3865: Set the emitted IR version to 0.6.29, and support 0.6.{29-30}.
    We also change the "rules" for what to put in `binaryEmitted` so
    that hopefully we can avoid this issue in the future.

--- ir/src/main/scala/org/scalajs/core/ir/ScalaJSVersions.scala
+++ ir/src/main/scala/org/scalajs/core/ir/ScalaJSVersions.scala
@@ -31,14 +31,22 @@ object ScalaJSVersions {
    *
    *  This should be either of:
    *  - a prior release version (i.e. "0.5.0", *not* "0.5.0-SNAPSHOT")
-   *  - `current`
+   *  - a copy-paste of the *rhs* of `current` (*not* a reference to the val
+   *    `current`, so that we notice a potential "-SNAPSHOT" suffix to be
+   *    replaced on release, to avoid issues like #3865).
    */
-  val binaryEmitted: String = current
+  val binaryEmitted: String = "0.6.29"
 
   /** Versions whose binary files we can support (used by deserializer) */
   val binarySupported: Set[String] = {
+    /* Note: 0.6.30 was never meant to be a valid IR version, but it Scala.js
+     * v0.6.30 was published with accidentally advertising that it emits IR
+     * version 0.6.30, while itself not being able to consume IR v0.6.29. We
+     * therefore advertise that we support consuming IR v0.6.30 although we
+     * emit v0.6.29.
+     */
     Set("0.6.0", "0.6.3", "0.6.4", "0.6.5", "0.6.6", "0.6.8", "0.6.13",
-        "0.6.14", "0.6.15", "0.6.17", binaryEmitted)
+        "0.6.14", "0.6.15", "0.6.17", "0.6.29", "0.6.30", binaryEmitted)
   }
 
   // Just to be extra safe
