61e7bfa25eb4296d5ac3ec1e5bd1889479be1e63
Tobias Schlatter <tobias@meisch.ch>
2019-11-22 17:36:52 +0100

    Restrict public API of ScalaJSCrossVersion
    We remove `scalaJSMapped` and `currentBinaryVersion`. Both of them
    seem unused in the public internet.

--- sbt-plugin/src/main/scala/org/scalajs/sbtplugin/ScalaJSCrossVersion.scala
+++ sbt-plugin/src/main/scala/org/scalajs/sbtplugin/ScalaJSCrossVersion.scala
@@ -17,20 +17,11 @@ import sbt._
 import org.scalajs.ir.ScalaJSVersions
 
 object ScalaJSCrossVersion {
-  val currentBinaryVersion = ScalaJSVersions.binaryCross
+  private val sjsPrefix = "sjs" + ScalaJSVersions.binaryCross + "_"
 
-  def scalaJSMapped(cross: CrossVersion): CrossVersion = cross match {
-    case CrossVersion.Disabled =>
-      CrossVersion.constant("sjs" + currentBinaryVersion)
-    case cross: sbt.librarymanagement.Constant =>
-      cross.withValue("sjs" + currentBinaryVersion + "_" + cross.value)
-    case cross: CrossVersion.Binary =>
-      cross.withPrefix("sjs" + currentBinaryVersion + "_" + cross.prefix)
-    case cross: CrossVersion.Full =>
-      cross.withPrefix("sjs" + currentBinaryVersion + "_" + cross.prefix)
-  }
+  val binary: CrossVersion =
+    CrossVersion.binaryWith(prefix = sjsPrefix, suffix = "")
 
-  val binary: CrossVersion = scalaJSMapped(CrossVersion.binary)
-
-  val full: CrossVersion = scalaJSMapped(CrossVersion.full)
+  val full: CrossVersion =
+    CrossVersion.fullWith(prefix = sjsPrefix, suffix = "")
 }
