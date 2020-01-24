09b584fa595f8cbbb131bf09e4405ec4262fa4d8
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-01-24 13:46:31 +0100

    Make CacheBox public.
    It is used in the types of public settings, so it must be public.
    
    Since we make it public, we must also make sure that it works even
    if `T` is nullable and that `null` is a valid value. So we change
    the implementation not to rely on `null` as a sentinel.

--- sbt-plugin/src/main/scala/org/scalajs/sbtplugin/CacheBox.scala
+++ sbt-plugin/src/main/scala/org/scalajs/sbtplugin/CacheBox.scala
@@ -17,18 +17,20 @@ package org.scalajs.sbtplugin
  *  A CacheBox is needed, once the cached result needs to depend on a task,
  *  since then it cannot simply be made a setting anymore.
  */
-private[sbtplugin] final class CacheBox[T] {
+final class CacheBox[T] {
+  private[this] var initialized: Boolean = _
   private[this] var value: T = _
 
   def ensure(f: => T): T = synchronized {
-    if (value == null) {
+    if (!initialized) {
       value = f
+      initialized = true
     }
     value
   }
 
   def foreach(f: T => Unit): Unit = synchronized {
-    if (value != null) {
+    if (initialized) {
       f(value)
     }
   }
