9814d5bd88904964ec06a3b492c7129d4c3de567
FabioPinheiro <fabiomgpinheiro@gmail.com>
2020-06-11 00:06:12 +0100

    Adapt SymbolTest to Dotty & Scala2 common syntax
    Dotty no longer supports symbol literals.
    Adapt SymbolTest to use application Symbol instead of symbol literal.
    Add test cases using symbol literal on SymbolTestScala2.
    
    Co-authored-by: Sébastien Doeraene <sjrdoeraene@gmail.com>

--- test-suite/shared/src/test/scala/org/scalajs/testsuite/scalalib/SymbolTest.scala
+++ test-suite/shared/src/test/scala/org/scalajs/testsuite/scalalib/SymbolTest.scala
@@ -23,36 +23,38 @@ class SymbolTest {
     def expectEqual(sym1: Symbol, sym2: Symbol): Unit = {
       assertTrue(sym1 eq sym2)
       assertEquals(sym2, sym1)
-      assertEquals(sym2, sym1)
       assertEquals(sym2.##, sym1.##)
     }
 
-    expectEqual('ScalaJS, Symbol("ScalaJS"))
-    expectEqual('$, Symbol("$"))
-    expectEqual('-, Symbol("-"))
+    expectEqual(Symbol("ScalaJS"), Symbol("ScalaJS"))
+    expectEqual(Symbol("$"), Symbol("$"))
+    expectEqual(Symbol("-"), Symbol("-"))
+
+    val scalajs = Symbol("ScalaJS")
+    expectEqual(scalajs, Symbol("ScalaJS"))
 
     val `42` = Symbol("42")
-    val map = Map[Symbol, Any](Symbol("ScalaJS") -> "Scala.js", '$ -> 1.2, `42` -> 42)
-    assertEquals("Scala.js", map('ScalaJS))
+    val map = Map[Symbol, Any](Symbol("ScalaJS") -> "Scala.js", Symbol("$") -> 1.2, `42` -> 42)
+    assertEquals("Scala.js", map(Symbol("ScalaJS")))
     assertEquals(1.2, map(Symbol("$")))
     assertEquals(42, map(Symbol("42")))
     assertEquals(42, map(`42`))
   }
 
   @Test def should_support_name(): Unit = {
-    val scalajs = 'ScalaJS
+    val scalajs = Symbol("ScalaJS")
 
     assertEquals("ScalaJS", scalajs.name)
     assertEquals("$", Symbol("$").name)
-    assertEquals("$$", '$$.name)
-    assertEquals("-", '-.name)
-    assertEquals("*", '*.name)
+    assertEquals("$$", Symbol("$$").name)
+    assertEquals("-", Symbol("-").name)
+    assertEquals("*", Symbol("*").name)
     assertEquals("'", Symbol("'").name)
     assertEquals("\"", Symbol("\"").name)
   }
 
   @Test def should_support_toString(): Unit = {
-    val scalajs = 'ScalaJS
+    val scalajs = Symbol("ScalaJS")
 
     val toStringUsesQuoteSyntax = {
       scalaVersion.startsWith("2.10.") ||
@@ -66,17 +68,17 @@ class SymbolTest {
     if (toStringUsesQuoteSyntax) {
       assertEquals("'ScalaJS", scalajs.toString)
       assertEquals("'$", Symbol("$").toString)
-      assertEquals("'$$", '$$.toString)
-      assertEquals("'-", '-.toString)
-      assertEquals("'*", '*.toString)
+      assertEquals("'$$", Symbol("$$").toString)
+      assertEquals("'-", Symbol("-").toString)
+      assertEquals("'*", Symbol("*").toString)
       assertEquals("''", Symbol("'").toString)
       assertEquals("'\"", Symbol("\"").toString)
     } else {
       assertEquals("Symbol(ScalaJS)", scalajs.toString)
       assertEquals("Symbol($)", Symbol("$").toString)
-      assertEquals("Symbol($$)", '$$.toString)
-      assertEquals("Symbol(-)", '-.toString)
-      assertEquals("Symbol(*)", '*.toString)
+      assertEquals("Symbol($$)", Symbol("$$").toString)
+      assertEquals("Symbol(-)", Symbol("-").toString)
+      assertEquals("Symbol(*)", Symbol("*").toString)
       assertEquals("Symbol(')", Symbol("'").toString)
       assertEquals("Symbol(\")", Symbol("\"").toString)
     }
