def4e7f82bca8bbbd0007f18e913898de9e43bd3
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-06-10 09:49:04 +0200

    Add an IDE stub for `emitter.PrivateLibHolder` in the linkerJS.
    In the JVM version of `linker`, `PrivateLibHolder` is a normal
    source file, but in JS it is generated by the build. Depending on
    whether Metals chose to associate `Emitter.scala` to the JVM or JS
    project, we could get spurious compile errors in the IDE because it
    would not find `PrivateLibHolder` in the JS project.
    
    We fix this by adding an IDE stub for `PrivateLibHolder` in the JS
    version of the linker, similarly to what we do for other generated
    sources.

---
+++ linker/js/src/main/scala-ide-stubs/org/scalajs/linker/backend/emitter/PrivateLibHolder.scala
@@ -0,0 +1,19 @@
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package org.scalajs.linker.backend.emitter
+
+import org.scalajs.linker.interface.IRFile
+
+object PrivateLibHolder {
+  val files: Seq[IRFile] = Nil
+}
