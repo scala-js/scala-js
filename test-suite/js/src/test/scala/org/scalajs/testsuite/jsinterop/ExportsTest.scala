acca4f996cddd569d2f4f0eaf98a876a33eb68d5
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-10-02 11:58:54 +0200

    Make the bug-compat aspect of the `WasPublicBeforeTyper` test Scala 2-only.
    Scala 3 does not expose that bug to begin with.

--- test-suite/js/src/test/scala/org/scalajs/testsuite/jsinterop/ExportsTest.scala
+++ test-suite/js/src/test/scala/org/scalajs/testsuite/jsinterop/ExportsTest.scala
@@ -1286,29 +1286,6 @@ class ExportsTest {
     testExposure(getJSObj2())
     testExposure(getJSObj3())
     testExposure(getJSObj4())
-
-    // Test that non js.Any classes were unaffected by the fix.
-
-    def getObj(): AnyRef = new {
-      val x1 = "x1"
-      var y1 = "y1"
-      def z1() = "z1"
-      private val x2 = "x2"
-      private var y2 = "y2"
-      private def z2() = "z2"
-      private[this] val x3 = "x3"
-      private[this] var y3 = "y3"
-      private[this] def z3() = "z3"
-    }
-
-    import scala.language.reflectiveCalls
-
-    val obj2 = getObj().asInstanceOf[{ val x1: String; var y1: String; def z1(): String }]
-
-    assertThrows(classOf[Throwable], obj2.x1)
-    assertThrows(classOf[Throwable], obj2.y1)
-    assertThrows(classOf[Throwable], obj2.y1 = "y1+")
-    assertThrows(classOf[Throwable], obj2.z1)
   }
 
   // @JSExportTopLevel
