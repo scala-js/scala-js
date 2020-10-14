4816650b95faac2338385ac4ba4e4f3abc780e29
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-10-14 18:28:03 +0200

    Version 1.3.0.

--- ir/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,8 +17,8 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.3.0-SNAPSHOT",
-    binaryEmitted = "1.3-SNAPSHOT"
+    current = "1.3.0",
+    binaryEmitted = "1.3"
 )
 
 /** Helper class to allow for testing of logic. */
