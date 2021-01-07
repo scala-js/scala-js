c6dc3f3b7fc6d59b0bc39dba68a9195bdc9577dc
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-01-07 23:06:44 +0900

    Add js.WeakRef and js.FinalizationRegistry
    Bumped Node.js to Node.js 14.6.0+ since FinalizationRegistry requires it

---
+++ test-suite/js/src/test/scala/org/scalajs/testsuite/library/FinalizationRegistryTest.scala
@@ -0,0 +1,36 @@
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package org.scalajs.testsuite.library
+
+import org.junit.Test
+
+import scala.scalajs.js
+
+class FinalizationRegistryTest {
+  @Test def testMethods(): Unit = {
+    val registry = new js.FinalizationRegistry[js.Date, String, Any]((heldValue: String) => ())
+
+    val obj1 = new js.Date()
+    registry.register(obj1, "foo")
+
+    val obj2 = new js.Date()
+    registry.register(obj2, "bar", obj2)
+
+    val obj3 = new js.Date()
+    val unregisterToken = new js.Object()
+    registry.register(obj3, "bar", unregisterToken)
+
+    registry.unregister(obj1)
+    registry.unregister(unregisterToken)
+  }
+}
