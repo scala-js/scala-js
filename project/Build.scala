4c82cfe37752a9de4dc2c8eaa51d7b779892c5a3
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-01-09 16:03:17 +0100

    Upgrade to GCC v20210106.
    This new version correctly prints spread operators with comma
    operators inside, i.e., `...(a, b)`.

--- project/Build.scala
+++ project/Build.scala
@@ -924,7 +924,7 @@ object Build {
     commonLinkerSettings _
   ).settings(
       libraryDependencies ++= Seq(
-          "com.google.javascript" % "closure-compiler" % "v20201102",
+          "com.google.javascript" % "closure-compiler" % "v20210106",
           "com.novocode" % "junit-interface" % "0.9" % "test",
           "com.google.jimfs" % "jimfs" % "1.1" % "test"
       ) ++ (
