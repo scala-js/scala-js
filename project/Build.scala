109841042c49dd5f8b730d60c2a7f25d9a945dd1
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-02-12 12:06:32 +0100

    Towards 1.5.1.

--- project/Build.scala
+++ project/Build.scala
@@ -233,7 +233,7 @@ object Build {
   val packageMinilib = taskKey[File]("Produces the minilib jar.")
 
   val previousVersions = List("1.0.0", "1.0.1", "1.1.0", "1.1.1",
-      "1.2.0", "1.3.0", "1.3.1", "1.4.0")
+      "1.2.0", "1.3.0", "1.3.1", "1.4.0", "1.5.0")
   val previousVersion = previousVersions.last
 
   val previousBinaryCrossVersion = CrossVersion.binaryWith("sjs1_", "")
