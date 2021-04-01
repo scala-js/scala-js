16e066d54fc1a6a97b13198f64946237dab1dee7
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-04-01 10:12:58 +0200

    Bump the version to 1.6.0-SNAPSHOT and the IR version to 1.6-SNAPSHOT.
    To accommodate the upcoming new configuration options in
    `ESFeatures`.
    
    We bump the IR version number as well because we will add a field
    to the `$linkingInfo` structure produced by the linker. In order
    for the newer version of the library to be able to rely on it, we
    need the guarantee that it gets linked with a newer linker.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,8 +17,8 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.5.2-SNAPSHOT",
-    binaryEmitted = "1.5"
+    current = "1.6.0-SNAPSHOT",
+    binaryEmitted = "1.6-SNAPSHOT"
 )
 
 /** Helper class to allow for testing of logic. */
