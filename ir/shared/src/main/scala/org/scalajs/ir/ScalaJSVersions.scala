6d0a0de410672ca0b9c6422089c670ba6f6ed448
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-08-03 13:40:59 +0200

    Version 1.7.0.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,8 +17,8 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.7.0-SNAPSHOT",
-    binaryEmitted = "1.7-SNAPSHOT"
+    current = "1.7.0",
+    binaryEmitted = "1.7"
 )
 
 /** Helper class to allow for testing of logic. */
