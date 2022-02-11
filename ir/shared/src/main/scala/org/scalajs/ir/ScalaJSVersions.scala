63154315ac54d4467c9944ef9b1aee8c1e068823
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-02-11 09:53:16 +0100

    Version 1.9.0.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.9.0-SNAPSHOT",
+    current = "1.9.0",
     binaryEmitted = "1.8"
 )
 
