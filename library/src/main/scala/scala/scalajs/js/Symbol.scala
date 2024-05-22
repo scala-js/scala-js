de89b486c344d49fffd184876c004b1bcce92c66
Daniel Moss <daniel.david.moss+git@gmail.com>
2024-05-22 20:10:38 +0100

    Annotate the new symbol with the ECMAScript 2018 badge
    Co-authored-by: Sébastien Doeraene <sjrdoeraene@gmail.com>

--- library/src/main/scala/scala/scalajs/js/Symbol.scala
+++ library/src/main/scala/scala/scalajs/js/Symbol.scala
@@ -67,7 +67,8 @@ object Symbol extends js.Object {
    */
   def keyFor(sym: js.Symbol): js.UndefOr[String] = js.native
 
-  /** The well-known symbol `@@asyncIterator`.
+  /** <span class="badge badge-ecma2018" style="float: right;">ECMAScript 2018</span>
+   *  The well-known symbol `@@asyncIterator`.
    *
    *  @group wellknownsyms
    */
