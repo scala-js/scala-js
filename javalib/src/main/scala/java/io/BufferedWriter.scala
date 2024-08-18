9227086bca0747aa447de7caa7b6679c1026740a
Thijs Broersen <thijsbroersen@gmail.com>
2024-08-18 00:19:53 +0000

    feedback changes, todo: add more tests

--- javalib/src/main/scala/java/io/BufferedWriter.scala
+++ javalib/src/main/scala/java/io/BufferedWriter.scala
@@ -1,3 +1,17 @@
+// Ported from Scala-native
+
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 package java.io
 
 class BufferedWriter(out: Writer, sz: Int) extends Writer {
@@ -6,9 +20,9 @@ class BufferedWriter(out: Writer, sz: Int) extends Writer {
 
   def this(out: Writer) = this(out, 4096)
 
-  private val buffer: Array[Char] = new Array[Char](sz)
-  private var pos: Int = 0
-  private var closed: Boolean = false
+  private[this] val buffer: Array[Char] = new Array[Char](sz)
+  private[this] var pos: Int = 0
+  private[this] var closed: Boolean = false
 
   def close(): Unit = if (!closed) {
     flush()
@@ -18,30 +32,38 @@ class BufferedWriter(out: Writer, sz: Int) extends Writer {
 
   def flush(): Unit = {
     ensureOpen()
-    out.write(buffer, 0, pos)
-    out.flush()
-    pos = 0
+    if (pos > 0) {
+      out.write(buffer, 0, pos)
+      out.flush()
+      pos = 0
+    }
   }
 
   def newLine(): Unit =
-    write(System.lineSeparator(), 0, System.lineSeparator().length)
+    write(System.lineSeparator())
 
-  override def write(c: Int): Unit =
-    write(Array(c.toChar), 0, 1)
+  override def write(c: Int): Unit = {
+    buffer.update(pos, c.toChar)
+    pos += 1
+    if (pos == sz)
+      flush()
+  }
 
   override def write(s: String, off: Int, len: Int): Unit =
     write(s.toCharArray, off, len)
 
   def write(cbuf: Array[Char], off: Int, len: Int): Unit = {
     ensureOpen()
-    val available = sz - pos
-    if (available >= len) {
-      System.arraycopy(cbuf, off, buffer, pos, len)
-      pos += len
-      if (pos == sz) flush()
-    } else {
-      write(cbuf, off, available)
-      write(cbuf, off + available, len - available)
+    if (len > 0 || off + len > 0) {
+      val available = sz - pos
+      if (available > len) {
+        System.arraycopy(cbuf, off, buffer, pos, len)
+        pos += len
+      } else {
+        flush()
+        out.write(cbuf, off, len)
+        out.flush()
+      }
     }
   }
 
--- test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/io/BufferedWriterTest.scala
+++ test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/io/BufferedWriterTest.scala
@@ -1,3 +1,17 @@
+// Ported from Scala-native
+
+/*
+ * Scala.js (https://www.scala-js.org/)
+ *
+ * Copyright EPFL.
+ *
+ * Licensed under Apache License 2.0
+ * (https://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 package org.scalajs.testsuite.javalib.io
 
 import java.io._
@@ -18,21 +32,21 @@ class BufferedWriterTest {
   }
 
   @Test def canWriteSmallChunksToBufferedWriter(): Unit = {
-    val stream = new ByteArrayOutputStream
-    val writer = new BufferedWriter(new OutputStreamWriter(stream))
+    val stringWriter = new StringWriter()
+    val writer = new BufferedWriter(stringWriter)
     val string = "Hello, world!"
     writer.write(string)
     writer.flush()
-    assertTrue(stream.toString == string)
+    assertTrue(stringWriter.toString == string)
   }
 
   @Test def canWriteChunkLargerThanBufferSizeToBufferedWriter(): Unit = {
-    val stream = new ByteArrayOutputStream
-    val writer = new BufferedWriter(new OutputStreamWriter(stream), 1)
+    val stringWriter = new StringWriter()
+    val writer = new BufferedWriter(stringWriter, 1)
     val string = "Hello, world!"
     writer.write(string)
     writer.flush()
-    assertTrue(stream.toString == string)
+    assertTrue(stringWriter.toString == string)
   }
 
   @Test def closingTwiceIsHarmless(): Unit = {
