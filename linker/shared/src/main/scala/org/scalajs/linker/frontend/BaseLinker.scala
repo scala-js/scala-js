8f0305e3bae8ab08ec3da73eff85bf3723fa7491
Sébastien Doeraene <sjrdoeraene@gmail.com>
2024-12-29 20:49:17 +0100

    Add a desugaring step in the base linker.
    Previously, the emitters and the optimizer all had to perform the
    same desugaring for `LinkTimeProperty` nodes. Instead, we now
    perform the desugaring in the `BaseLinker`, after the reachability
    analysis.
    
    The reachability analysis records whether each method needs
    desugaring or not. For those that do, we cache the resulting
    desugaring. Methods that do not require desugaring are not
    processed, and so incur no additional cost.
    
    The machinery is heavy. It definitely outweighs the benefits in
    terms of duplication for `LinkTimeProperty` alone. However, the
    same machinery will be used to desugar `NewLambda` nodes. This
    commit serves as a stepping stone in that direction.

--- linker/shared/src/main/scala/org/scalajs/linker/frontend/BaseLinker.scala
+++ linker/shared/src/main/scala/org/scalajs/linker/frontend/BaseLinker.scala
@@ -12,6 +12,7 @@
 
 package org.scalajs.linker.frontend
 
+import scala.collection.mutable
 import scala.concurrent._
 
 import org.scalajs.logging._
@@ -23,7 +24,7 @@ import org.scalajs.linker.checker._
 import org.scalajs.linker.analyzer._
 
 import org.scalajs.ir
-import org.scalajs.ir.Names.ClassName
+import org.scalajs.ir.Names._
 import org.scalajs.ir.Trees.{ClassDef, MethodDef}
 import org.scalajs.ir.Version
 
@@ -38,6 +39,9 @@ final class BaseLinker(config: CommonPhaseConfig, checkIR: Boolean) {
   private val irLoader = new FileIRLoader
   private val analyzer =
     new Analyzer(config, initial = true, checkIR = checkIR, failOnError = true, irLoader)
+  private val desugarTransformer = new DesugarTransformer(config.coreSpec)
+  private val desugaredClassCaches =
+    new java.util.concurrent.ConcurrentHashMap[ClassName, DesugaredClassCache]
   private val methodSynthesizer = new MethodSynthesizer(irLoader)
 
   def link(irInput: Seq[IRFile],
@@ -74,6 +78,17 @@ final class BaseLinker(config: CommonPhaseConfig, checkIR: Boolean) {
 
   private def assemble(moduleInitializers: Seq[ModuleInitializer],
       analysis: Analysis)(implicit ec: ExecutionContext): Future[LinkingUnit] = {
+
+    /* Remove stale entries from the desugared class cache.
+     * Note that we do not remove individual stale *methods* inside a live
+     * class. This has the potential for memory leaks if methods requiring
+     * desugaring keep being added and removed across runs.
+     * It does not seem worth guarding against, though.
+     */
+    desugaredClassCaches.keySet().removeIf { (className: ClassName) =>
+      !analysis.classInfos.contains(className)
+    }
+
     def assembleClass(info: ClassInfo) = {
       val version = irLoader.irFileVersion(info.className)
       val syntheticMethods = methodSynthesizer.synthesizeMembers(info, analysis)
@@ -82,7 +97,12 @@ final class BaseLinker(config: CommonPhaseConfig, checkIR: Boolean) {
         classDef <- irLoader.loadClassDef(info.className)
         syntheticMethods <- syntheticMethods
       } yield {
-        BaseLinker.linkClassDef(classDef, version, syntheticMethods, analysis)
+        val desugaredClassCache = desugaredClassCaches.computeIfAbsent(info.className, {
+          (className: ClassName) =>
+            new DesugaredClassCache(desugarTransformer)
+        })
+        BaseLinker.linkClassDef(classDef, version, syntheticMethods, analysis,
+            Some(desugaredClassCache))
       }
     }
 
@@ -105,12 +125,107 @@ final class BaseLinker(config: CommonPhaseConfig, checkIR: Boolean) {
 
 private[frontend] object BaseLinker {
 
+  private final class DesugarTransformer(coreSpec: CoreSpec)
+      extends ir.Transformers.ClassTransformer {
+
+    import ir.Trees._
+
+    override def transform(tree: Tree): Tree = {
+      tree match {
+        case prop: LinkTimeProperty =>
+          coreSpec.linkTimeProperties.transformLinkTimeProperty(prop)
+
+        case _ =>
+          super.transform(tree)
+      }
+    }
+
+    /* Transfer Version from old members to transformed members.
+     * We can do this because the transformation only depends on the
+     * `coreSpec`, which is immutable.
+     */
+
+    override def transformMethodDef(methodDef: MethodDef): MethodDef = {
+      val newMethodDef = super.transformMethodDef(methodDef)
+      newMethodDef.copy()(newMethodDef.optimizerHints, methodDef.version)(newMethodDef.pos)
+    }
+
+    override def transformJSConstructorDef(jsConstructor: JSConstructorDef): JSConstructorDef = {
+      val newJSConstructor = super.transformJSConstructorDef(jsConstructor)
+      newJSConstructor.copy()(newJSConstructor.optimizerHints, jsConstructor.version)(
+          newJSConstructor.pos)
+    }
+
+    override def transformJSMethodDef(jsMethodDef: JSMethodDef): JSMethodDef = {
+      val newJSMethodDef = super.transformJSMethodDef(jsMethodDef)
+      newJSMethodDef.copy()(newJSMethodDef.optimizerHints, jsMethodDef.version)(
+          newJSMethodDef.pos)
+    }
+
+    override def transformJSPropertyDef(jsPropertyDef: JSPropertyDef): JSPropertyDef = {
+      val newJSPropertyDef = super.transformJSPropertyDef(jsPropertyDef)
+      newJSPropertyDef.copy()(jsPropertyDef.version)(newJSPropertyDef.pos)
+    }
+  }
+
+  private final class DesugaredClassCache(desugarTransformer: DesugarTransformer) {
+    import ir.Trees._
+
+    private val methodCache = mutable.Map.empty[(MemberNamespace, MethodName), MethodDef]
+
+    def desugarMethod(method: MethodDef): MethodDef = {
+      if (method.version == Version.Unversioned) {
+        desugarTransformer.transformMethodDef(method)
+      } else {
+        val key = (method.flags.namespace, method.methodName)
+        methodCache.get(key) match {
+          case Some(desugared) if desugared.version.sameVersion(method.version) =>
+            desugared
+          case _ =>
+            val desugared = desugarTransformer.transformMethodDef(method)
+            methodCache(key) = desugared
+            desugared
+        }
+      }
+    }
+
+    def desugarJSConstructor(jsConstructor: Option[JSConstructorDef]): Option[JSConstructorDef] = {
+      // We do not actually cache the desugaring of JS members
+      jsConstructor.map(desugarTransformer.transformJSConstructorDef(_))
+    }
+
+    def desugarJSMethodProps(jsMethodProps: List[JSMethodPropDef]): List[JSMethodPropDef] = {
+      // We do not actually cache the desugaring of JS members
+      jsMethodProps.map(desugarTransformer.transformJSMethodPropDef(_))
+    }
+
+    def desugarTopLevelExport(topLevelExport: TopLevelExportDef): TopLevelExportDef = {
+      // We do not actually cache top-level exports
+      desugarTransformer.transformTopLevelExportDef(topLevelExport)
+    }
+  }
+
+  /** Takes a ClassDef and DCE infos to construct a stripped down LinkedClass.
+   */
+  private[frontend] def refineClassDef(classDef: ClassDef, version: Version,
+      analysis: Analysis): (LinkedClass, List[LinkedTopLevelExport]) = {
+    linkClassDef(classDef, version, syntheticMethodDefs = Nil, analysis,
+        desugaredClassCache = None)
+  }
+
   /** Takes a ClassDef and DCE infos to construct a stripped down LinkedClass.
    */
   private[frontend] def linkClassDef(classDef: ClassDef, version: Version,
-      syntheticMethodDefs: List[MethodDef],
-      analysis: Analysis): (LinkedClass, List[LinkedTopLevelExport]) = {
+      syntheticMethodDefs: List[MethodDef], analysis: Analysis,
+      desugaredClassCache: Option[DesugaredClassCache]): (LinkedClass, List[LinkedTopLevelExport]) = {
     import ir.Trees._
+
+    def requireDesugaredClassCache(): DesugaredClassCache = {
+      desugaredClassCache.getOrElse {
+        throw new AssertionError(
+            s"Unexpected desugaring needed in refiner in class ${classDef.className.nameString}")
+      }
+    }
 
     val classInfo = analysis.classInfos(classDef.className)
 
@@ -127,25 +242,32 @@ private[frontend] object BaseLinker {
         classInfo.isAnySubclassInstantiated
     }
 
-    val methods = classDef.methods.filter { m =>
-      val methodInfo =
-        classInfo.methodInfos(m.flags.namespace)(m.methodName)
+    val methods: List[MethodDef] = classDef.methods.iterator
+      .map(m => m -> classInfo.methodInfos(m.flags.namespace)(m.methodName))
+      .filter(_._2.isReachable)
+      .map { case (m, info) =>
+        assert(m.body.isDefined,
+            s"The abstract method ${classDef.name.name}.${m.methodName} is reachable.")
+        if (!info.needsDesugaring)
+          m
+        else
+          requireDesugaredClassCache().desugarMethod(m)
+      }
+      .toList
 
-      val reachable = methodInfo.isReachable
-      assert(m.body.isDefined || !reachable,
-          s"The abstract method ${classDef.name.name}.${m.methodName} " +
-          "is reachable.")
+    val (jsConstructor, jsMethodProps) = if (classInfo.isAnySubclassInstantiated) {
+      val anyJSMemberNeedsDesugaring = classInfo.anyJSMemberNeedsDesugaring
 
-      reachable
+      if (!anyJSMemberNeedsDesugaring) {
+        (classDef.jsConstructor, classDef.jsMethodProps)
+      } else {
+        val cache = requireDesugaredClassCache()
+        (cache.desugarJSConstructor(classDef.jsConstructor),
+            cache.desugarJSMethodProps(classDef.jsMethodProps))
+      }
+    } else {
+      (None, Nil)
     }
-
-    val jsConstructor =
-      if (classInfo.isAnySubclassInstantiated) classDef.jsConstructor
-      else None
-
-    val jsMethodProps =
-      if (classInfo.isAnySubclassInstantiated) classDef.jsMethodProps
-      else Nil
 
     val jsNativeMembers = classDef.jsNativeMembers
       .filter(m => classInfo.jsNativeMembersUsed.contains(m.name.name))
@@ -186,6 +308,9 @@ private[frontend] object BaseLinker {
     } yield {
       val infos = analysis.topLevelExportInfos(
         (ModuleID(topLevelExport.moduleID), topLevelExport.topLevelExportName))
+      val desugared =
+        if (!infos.needsDesugaring) topLevelExport
+        else requireDesugaredClassCache().desugarTopLevelExport(topLevelExport)
       new LinkedTopLevelExport(classDef.className, topLevelExport,
           infos.staticDependencies.toSet, infos.externalDependencies.toSet)
     }
