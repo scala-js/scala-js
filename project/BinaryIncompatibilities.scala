5eb1546d574301094f23deb24538687c182c888c
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-01-06 15:49:14 +0100

    Towards 1.18.1.

--- project/BinaryIncompatibilities.scala
+++ project/BinaryIncompatibilities.scala
@@ -5,56 +5,9 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object BinaryIncompatibilities {
   val IR = Seq(
-    // !!! Breaking, OK in minor release
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Printers#IRTreePrinter.print"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$ArrayLength"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$ArrayLength$"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$Clone"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$Clone$"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$GetClass"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$GetClass$"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$IdentityHashCode"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$IdentityHashCode$"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$JSLinkingInfo"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$JSLinkingInfo$"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$LabelIdent"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$LabelIdent$"),
-    ProblemFilters.exclude[Problem]("org.scalajs.ir.Trees#Labeled.*"),
-    ProblemFilters.exclude[Problem]("org.scalajs.ir.Trees#Return.*"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$This"),
-    ProblemFilters.exclude[MissingTypesProblem]("org.scalajs.ir.Trees$This$"),
-    ProblemFilters.exclude[IncompatibleResultTypeProblem]("org.scalajs.ir.Trees#This.apply"),
-    ProblemFilters.exclude[IncompatibleMethTypeProblem]("org.scalajs.ir.Trees#This.unapply"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$Throw"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$Throw$"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$UnwrapFromThrowable"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$UnwrapFromThrowable$"),
-    ProblemFilters.exclude[Problem]("org.scalajs.ir.Trees#VarRef.*"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$WrapAsThrowable"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Trees$WrapAsThrowable$"),
-    ProblemFilters.exclude[IncompatibleResultTypeProblem]("org.scalajs.ir.Trees#*.tpe"),
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Types$NoType$"),
-
-    // !!! Breaking, OK in minor release
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Transformers#Transformer.transform"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Transformers#Transformer.transformExpr"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Transformers#Transformer.transformExprOrJSSpread"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Transformers#Transformer.transformStat"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Transformers#Transformer.transformStats"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Trees#Transient#Value.transform"),
-    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.scalajs.ir.Trees#Transient#Value.transform"),
-
-    // private, not an issue
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Serializers#Deserializer.readLabelIdent"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Serializers#Serializer.writeLabelIdent"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Hashers#TreeHasher.mixLabelIdent"),
   )
 
   val Linker = Seq(
-    // !!! Breaking, OK in minor release
-    exclude[DirectMissingMethodProblem]("org.scalajs.linker.standard.ModuleSet.coreSpec"),
-    exclude[DirectMissingMethodProblem]("org.scalajs.linker.standard.ModuleSet.this"),
-    exclude[DirectMissingMethodProblem]("org.scalajs.linker.standard.LinkerBackend.verifyModuleSet"),
   )
 
   val LinkerInterface = Seq(
