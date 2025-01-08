2cdb10b08a123e8f0097b89915d318e5d80aa84e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-01-08 17:47:01 +0100

    Towards 1.18.2.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.18.1",
+    current = "1.18.2-SNAPSHOT",
     binaryEmitted = "1.18"
 )
 
