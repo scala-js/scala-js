7a05348096103cc1bfecfbd941508316f974d91e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-01-10 10:45:08 +0100

    Bump the IR version to 1.19-SNAPSHOT for the upcoming changes.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -18,7 +18,7 @@ import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
     current = "1.19.0-SNAPSHOT",
-    binaryEmitted = "1.18"
+    binaryEmitted = "1.19-SNAPSHOT"
 )
 
 /** Helper class to allow for testing of logic. */
