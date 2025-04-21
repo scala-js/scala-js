b53a090206eb72be2c94893394297503106d0f04
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-04-21 11:41:44 +0200

    Towards 1.19.1.

--- project/BinaryIncompatibilities.scala
+++ project/BinaryIncompatibilities.scala
@@ -5,56 +5,12 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object BinaryIncompatibilities {
   val IR = Seq(
-    // !!! Breaking, OK in minor release
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.*Class"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.ClassInitializerName"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.DefaultModuleID"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.HijackedClasses"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.NoArgConstructorName"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.ObjectArgConstructorName"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Names.StaticInitializerName"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types.BoxedClassToPrimType"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types.PrimTypeToBoxedClass"),
-
-    // !!! Breaking, OK in minor release
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.InvalidIRException.tree"),
-    ProblemFilters.exclude[Problem]("org.scalajs.ir.Trees#Closure.*"),
-
-    // !!! Breaking, PrimRef is not a case class anymore
-    ProblemFilters.exclude[MissingTypesProblem]("org.scalajs.ir.Types$PrimRef"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.canEqual"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.productArity"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.productElement"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.productElementName"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.productElementNames"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.productIterator"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.productPrefix"),
-    ProblemFilters.exclude[IncompatibleResultTypeProblem]("org.scalajs.ir.Types#PrimRef.unapply"),
-
-    // !!! Breaking I guess ... we used to leak public things out of a `case class` with a private[ir] constructor
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.this"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.apply"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.copy"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimRef.copy$default$1"),
-    ProblemFilters.exclude[MissingTypesProblem]("org.scalajs.ir.Types$PrimRef$"),
-
-    // constructor of a sealed abstract class, not an issue
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Types#PrimTypeWithRef.this"),
-
-    // private, not an issue
-    ProblemFilters.exclude[MissingClassProblem]("org.scalajs.ir.Serializers$Deserializer$BodyHack5Transformer$"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.ir.Serializers#Hacks.use*"),
   )
 
   val Linker = Seq(
-    // !!! Breaking, OK in minor release
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.linker.standard.LinkedClass.this"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.linker.standard.LinkedTopLevelExport.this"),
   )
 
   val LinkerInterface = Seq(
-    // private, not an issue
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.scalajs.linker.interface.Semantics.this"),
   )
 
   val SbtPlugin = Seq(
@@ -64,10 +20,6 @@ object BinaryIncompatibilities {
   )
 
   val Library = Seq(
-    // Changes covered by a deserialization hack (and the code cannot be used on the JVM, such as in macros)
-    ProblemFilters.exclude[AbstractClassProblem]("scala.scalajs.runtime.AnonFunction*"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.scalajs.runtime.AnonFunction*.this"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.scalajs.runtime.AnonFunction*.apply"),
   )
 
   val TestInterface = Seq(
