9e0c2463b4fed90404b02d2208848cce73f0236e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-09-05 23:16:57 +0200

    Version 1.20.1.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.20.1-SNAPSHOT",
+    current = "1.20.1",
     binaryEmitted = "1.20"
 )
 
