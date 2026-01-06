a314021fdf8bf74f0c6efd3455c3c7069c11bb47
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-01-06 10:22:22 +0100

    Bump the version to 1.21.0-SNAPSHOT for the upcoming changes.

--- ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
+++ ir/shared/src/main/scala/org/scalajs/ir/ScalaJSVersions.scala
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap
 import scala.util.matching.Regex
 
 object ScalaJSVersions extends VersionChecks(
-    current = "1.20.3-SNAPSHOT",
+    current = "1.21.0-SNAPSHOT",
     binaryEmitted = "1.20"
 )
 
