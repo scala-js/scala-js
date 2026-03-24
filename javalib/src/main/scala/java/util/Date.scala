97a1e0bab7320a42497b69651dc2097cf072cded
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-03-24 10:16:24 +0100

    Implement ju.Date.parse ourselves.
    Because the format specified by the JDK is actually not guaranteed
    to be supported by the JS engine. The only guaranteed format is not
    even valid for the JDK. According to the ECMAScript spec, JS engines
    are *allowed* to handle unspecified formats in addition to the
    specified one.

--- javalib/src/main/scala/java/util/Date.scala
+++ javalib/src/main/scala/java/util/Date.scala
@@ -12,6 +12,8 @@
 
 package java.util
 
+import scala.annotation.switch
+
 import java.lang.Cloneable
 import java.time.Instant
 import java.util.function._
@@ -163,12 +165,61 @@ object Date {
    */
   private final val MaxMillis = 8640000000000000L
 
+  private final val MSPerMinute = 60000
+
+  /** Marker for end-of-string in parsing; distinct from every `Char`. */
+  private final val EOF = -1
+
   private val Days = Array(
       "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
 
   private val Months = Array(
       "Jan", "Feb", "Mar", "Apr", "May", "Jun",
       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+  /** All the "words" for which `parse` tries a prefix, in order of precedence. */
+  private val ParsePrefixWords: Array[String] = Array(
+      "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY",
+      "AM", "PM",
+      "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
+      "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"
+  )
+
+  /** The current year, used to interpret small years in `parse`.
+   *
+   *  It is explicitly stored, rather than recomputed, because the JavaDoc
+   *  says the computation is relative to
+   *  > the time when the Date class is initialized
+   *
+   *  We don't follow the exact initialization time, but at least this way we
+   *  preserve the fact that only one `currentYear` is observed through the
+   *  lifetime of the application.
+   *
+   *  We make it a `lazy val` for DCE purposes. If a codebase does not use
+   *  `parse` (and it shouldn't), then we can DCE `currentYear` away. It uses
+   *  JS interop and therefore has side effects, so the optimizer cannot remove
+   *  it if it's a `val`.
+   */
+  private lazy val currentYear: Int =
+    new js.Date().getFullYear().toInt
+
+  private def resolveYear(number: Int): Int = {
+    /* > If the recognized year number is less than 100, it is interpreted as an
+     * > abbreviated year relative to a century of which dates are within 80 years
+     * > before and 19 years after the time when the Date class is initialized.
+     */
+    if (number >= 100) {
+      number
+    } else {
+      val relCenturyStart = currentYear - 80
+      val absCenturyStart = relCenturyStart - Integer.remainderUnsigned(relCenturyStart, 100)
+      val candidate = absCenturyStart + number
+      if (candidate >= relCenturyStart)
+        candidate
+      else
+        candidate + 100
+    }
+  }
 
   private def pad0(i: Int): String = {
     val str = "" + i
@@ -191,7 +242,245 @@ object Date {
   }
 
   @Deprecated
-  def parse(string: String): Long = safeGetTime(new js.Date(string))
+  def parse(string: String): Long = {
+    /* Parse the string according to
+     * https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/Date.html#parse(java.lang.String)
+     * Comments starting with '>' are quoted from the JavaDoc.
+     */
+
+    /* We can use -1 as absent value for these fields because they cannot
+     * be parsed as negative values.
+     */
+    var year = -1 // full year (not 1900-based)
+    var month = -1
+    var dayOfMonth = -1
+    var hour = -1
+    var minute = -1
+    var second = -1
+
+    /* The timezone offset (in minutes) can be negative, but is parsed from a
+     * positive int and therefore cannot be Int.MinValue.
+     */
+    var timezoneOffset = Int.MinValue
+
+    @inline def isASCIIDigit(c: Char): Boolean =
+      c >= '0' && c <= '9'
+
+    @inline def isASCIILetter(c: Char): Boolean =
+      (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+
+    def fail(): Nothing =
+      throw new IllegalArgumentException(string)
+
+    val len = string.length()
+    var i = 0
+    while (i != len) {
+      val start = i
+      val c = string.charAt(start)
+      i += 1
+
+      if (isASCIIDigit(c)) {
+        // Parse the whole number
+        while (i != len && isASCIIDigit(string.charAt(i))) {
+          i += 1
+        }
+        val number = Integer.parseInt(string.substring(start, i))
+
+        // Look behind and ahead
+        val prev = if (start == 0) EOF else string.charAt(start - 1).toInt
+        val next = if (i == len) EOF else string.charAt(i).toInt
+
+        // Decide what to do with the number
+        if ((prev == '+' || prev == '-') && year != -1) {
+          /* > If a number is preceded by + or - and a year has already been
+           * > recognized, then the number is a time-zone offset.
+           */
+          if (timezoneOffset != Int.MinValue && timezoneOffset != 0)
+            fail() // out of spec
+          val signedNumber = if (prev == '-') -number else number
+          if (number < 24) {
+            timezoneOffset = 60 * signedNumber
+          } else {
+            // convert HHmm (= 100*HH + mm) to 60*HH + mm
+            val tzHours = signedNumber / 100 // for once, actually use truncating signed division
+            val tzMinutes = signedNumber - 100 * tzHours
+            timezoneOffset = (60 * tzHours) + tzMinutes
+          }
+        } else {
+          /* For non-timezone fields, we refuse to recognize the same field twice.
+           * This is not clearly specified, but it's the "smallest deviation"
+           * from the spec that makes all tests pass.
+           */
+
+          /* > The number is regarded as a year number if one of the following conditions is true:
+           * > - The number is equal to or greater than 70 and followed by a
+           * >   space, comma, slash, or end of string.
+           * > - The number is less than 70, and both a month and a day of the
+           * >   month have already been recognized.
+           *
+           * These conditions are identified in a few places below, and sent into
+           * this function.
+           */
+          @inline def interpretAsYear(): Unit = {
+            // inlined to keep `year` as a (non-captured) local var
+            if (year == -1)
+              year = resolveYear(number) // but outlined not to duplicate code
+            else
+              fail()
+          }
+
+          (next: @switch) match {
+            case ':' =>
+              /* > If the number is followed by a colon, it is regarded as an hour,
+               * > unless an hour has already been recognized, in which case it is
+               * > regarded as a minute.
+               */
+              if (hour == -1)
+                hour = number
+              else if (minute == -1)
+                minute = number
+              else
+                fail()
+
+            case '/' =>
+              /* > If the number is followed by a slash, it is regarded as a month
+               * > (it is decreased by 1 to produce a number in the range 0 to 11),
+               * > unless a month has already been recognized, in which case it is
+               * > regarded as a day of the month.
+               */
+              if (number >= 70)
+                interpretAsYear()
+              else if (month == -1)
+                month = number - 1
+              else if (dayOfMonth == -1)
+                dayOfMonth = number
+              else
+                fail()
+
+            case '\t' | '\n' | '\f' | '\r' | ' ' | ',' | '-' | EOF =>
+              /* > If the number is followed by whitespace, a comma, a hyphen, or
+               * > end of string, then if an hour has been recognized but not a
+               * > minute, it is regarded as a minute; otherwise, if a minute has
+               * > been recognized but not a second, it is regarded as a second;
+               * > otherwise, it is regarded as a day of the month.
+               */
+              if (number >= 70 && (next == ' ' || next == ',' || next == EOF))
+                interpretAsYear()
+              else if (hour != -1 && minute == -1)
+                minute = number
+              else if (minute != -1 && second == -1)
+                second = number
+              else if (dayOfMonth == -1)
+                dayOfMonth = number
+              else if (month != -1 && number < 70)
+                interpretAsYear()
+              else
+                fail()
+
+            case _ =>
+              // Otherwise fail (not clearly specified)
+              fail()
+          }
+        }
+      } else if (isASCIILetter(c)) {
+        // Parse the whole ASCII word
+        while (i != len && isASCIILetter(string.charAt(i))) {
+          i += 1
+        }
+        if (i == start + 1)
+          fail() // out of spec, but apparently the JVM wants at least 2 letters here
+        val word = string.substring(start, i).toUpperCase()
+
+        // Decide what to do with it
+
+        val prefixWords = ParsePrefixWords // local copy
+        val prefixWordsLen = prefixWords.length
+        var prefixIndex = 0
+        while (prefixIndex != prefixWordsLen && !prefixWords(prefixIndex).startsWith(word)) {
+          prefixIndex += 1
+        }
+
+        if (prefixIndex < 7) {
+          // day of week, ignore
+        } else if (prefixIndex < 9) {
+          /* AM (7) or PM (8)
+           * > A word that matches AM, ignoring case, is ignored.
+           * > A word that matches PM, ignoring case, adds 12 to the hour .
+           * > [for both:] (but the parse fails if an hour has not been
+           * > recognized or is less than 1 or greater than 12).
+           */
+          if (hour < 1 || hour > 12) // hour < 1 includes not recognized
+            fail()
+          if (prefixIndex == 8)
+            hour += 12
+        } else if (prefixIndex < prefixWordsLen) {
+          // named month
+          month = prefixIndex - 9
+        } else {
+          // one of the explicitly known timezones, otherwise fail
+          timezoneOffset = word match {
+            case "GMT" | "UT" | "UTC" => 0
+            case "EDT"                => -4 * 60
+            case "EST" | "CDT"        => -5 * 60
+            case "CST" | "MDT"        => -6 * 60
+            case "MST" | "PDT"        => -7 * 60
+            case "PST"                => -8 * 60
+            case _                    => fail()
+          }
+        }
+      } else {
+        (c: @switch) match {
+          case '\t' | '\n' | '\f' | '\r' | ' ' | ',' | '+' | '-' | ':' | '/' =>
+            // ignore
+            ()
+          case '(' =>
+            // Ignore all characters until a matching ')'
+            var nesting = 1
+            while (i != len && nesting > 0) {
+              string.charAt(i) match {
+                case '(' => nesting += 1
+                case ')' => nesting -= 1
+                case _   => ()
+              }
+              i += 1
+            }
+          case _ =>
+            fail()
+        }
+      }
+    }
+
+    // We need all the components of a date, otherwise fail
+    if (year == -1 || month == -1 || dayOfMonth == -1)
+      fail()
+
+    /* However, time components are optional and default to 0.
+     * With our algorithm above, hour-not-set implies minute-not-set and
+     * minute-not-set implies second-not-set.
+     * It's not straightforward from the spec, though, so we process them
+     * separately anyway.
+     */
+    if (hour == -1)
+      hour = 0
+    if (minute == -1)
+      minute = 0
+    if (second == -1)
+      second = 0
+
+    if (timezoneOffset != Int.MinValue) {
+      /* > If a time zone or time-zone offset has been recognized, then the year,
+       * > month, day of month, hour, minute, and second are interpreted in UTC
+       * > and then the time-zone offset is applied.
+       */
+      val utc = UTC(year - 1900, month, dayOfMonth, hour, minute, second)
+      utc - (timezoneOffset * MSPerMinute).toLong
+    } else {
+      /* > Otherwise, the year, month, day of month, hour, minute, and second
+       * > are interpreted in the local time zone.
+       */
+      safeGetTime(new js.Date(year, month, dayOfMonth, hour, minute, second, 0))
+    }
+  }
 
   private def safeGetTime(date: js.Date): Long =
     safeGetTime(date.getTime())
