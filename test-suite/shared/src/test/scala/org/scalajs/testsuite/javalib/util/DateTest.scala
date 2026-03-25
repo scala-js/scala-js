237afa0c65782e11483afd6145be56c113612eff
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-03-25 17:47:18 +0100

    Harden tests for ju.Date.
    * Use explicit timestamps in most tests, since that
      is the primitive underlying data of our `ju.Date`.
    * Add tests for the constructors taking date fields.
    * Consolidate tests for comparisons.

--- test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/util/DateTest.scala
+++ test-suite/shared/src/test/scala/org/scalajs/testsuite/javalib/util/DateTest.scala
@@ -23,64 +23,86 @@ import org.scalajs.testsuite.utils.Platform.executingInJVM
 
 /** tests the implementation of the java standard library Date */
 class DateTest {
+  private final val MSPerDay = 24 * 60 * 60 * 1000
 
-  @Test def compareTo(): Unit = {
-    def compare(x: Date, y: Date): Int =
-      x.compareTo(y)
-
-    assertTrue(compare(new Date(97, 11, 5, 0, 0), new Date(98, 11, 5, 0, 0)) < 0)
-    assertTrue(compare(new Date(98, 11, 5, 0, 0), new Date(97, 11, 5, 0, 0)) > 0)
-    assertEquals(0, compare(new Date(97, 11, 5, 0, 0), new Date(97, 11, 5)))
-    assertTrue(compare(new Date(97, 11, 5, 0, 0), new Date(97, 11, 5, 0, 1)) < 0)
-    assertEquals(0, compare(new Date(97, 11, 5), new Date(97, 11, 5, 0, 0)))
+  @Test def fromUTC(): Unit = {
+    assertEquals(-2209075200000L, Date.UTC(1899 - 1900, 11, 31, 0, 0, 0))
+    assertEquals(878534607000L, Date.UTC(97, 10, 3, 5, 23, 27))
+    assertEquals(-2145542331000L, Date.UTC(2, 0, 5, 8, 1, 9))
+    assertEquals(29348715784000L, Date.UTC(2900 - 1900, 0, 9, 5, 3, 4))
   }
 
-  @Test def comparable(): Unit = {
-    def compare(x: Any, y: Any): Int =
-      x.asInstanceOf[Comparable[Any]].compareTo(y)
+  @Test def fromLocalFields(): Unit = {
+    def test(expectedUTC: Long, date: Date): Unit = {
+      // Whatever the local timezone, we should not be more than 1 day away from UTC
+      assertTrue(Math.abs(date.getTime() - expectedUTC) <= MSPerDay)
+    }
 
-    assertTrue(compare(new Date(97, 11, 5, 0, 0), new Date(98, 11, 5, 0, 0)) < 0)
-    assertTrue(compare(new Date(98, 11, 5, 0, 0), new Date(97, 11, 5, 0, 0)) > 0)
-    assertEquals(0, compare(new Date(97, 11, 5, 0, 0), new Date(97, 11, 5)))
-    assertTrue(compare(new Date(97, 11, 5, 0, 0), new Date(97, 11, 5, 0, 1)) < 0)
-    assertEquals(0, compare(new Date(97, 11, 5), new Date(97, 11, 5, 0, 0)))
+    test(-2209075200000L, new Date(1899 - 1900, 11, 31))
+    test(878534580000L, new Date(97, 10, 3, 5, 23))
+    test(-2145542331000L, new Date(2, 0, 5, 8, 1, 9))
+    test(29348715784000L, new Date(2900 - 1900, 0, 9, 5, 3, 4))
+  }
+
+  @Test def comparisons(): Unit = {
+    def assertCompare(expectedSign: Int, tx: Long, ty: Long): Unit = {
+      val x = new Date(tx)
+      val y = new Date(ty)
+      assertEquals(expectedSign, Integer.signum(x.compareTo(y)))
+
+      // also check that ju.Date implements Comparable
+      val cx: Comparable[Date] = x
+      assertEquals(expectedSign, Integer.signum(cx.compareTo(y)))
+
+      assertEquals(expectedSign > 0, x.after(y))
+      assertEquals(expectedSign < 0, x.before(y))
+    }
+
+    assertCompare(-1, 881280000000L, 912816000000L)
+    assertCompare(1, 912816000000L, 881280000000L)
+    assertCompare(0, 881280000000L, 881280000000L)
+    assertCompare(-1, -912816000000L, 881280000000L)
+    assertCompare(1, 881280000000L, -912816000000L)
+    assertCompare(-1, Long.MinValue, 0L)
+    assertCompare(1, Long.MaxValue, 0L)
   }
 
   @Test def parseStrings(): Unit = {
-    def test(s: String, v: Date): Unit = {
-      assertEquals(0, new Date(s).compareTo(v))
-      assertEquals(0, Date.parse(s).compareTo(v.getTime))
+    def test(expected: Long, s: String): Unit = {
+      assertEquals(s, expected, new Date(s).getTime())
+      assertEquals(s, expected, Date.parse(s))
     }
 
-    test("Nov 5 1997 5:23:27 GMT", new Date(Date.UTC(97, 10, 5, 5, 23, 27)))
-    test("Nov 1 1997 GMT", new Date(Date.UTC(97, 10, 1, 0, 0, 0)))
-    test("Jan 1 1970 18:11:01 GMT", new Date(Date.UTC(70, 0, 1, 18, 11, 1)))
+    def testFailure(s: String): Unit = {
+      assertThrows(classOf[IllegalArgumentException], new Date(s))
+      assertThrows(classOf[IllegalArgumentException], Date.parse(s))
+    }
 
-    assertThrows(classOf[IllegalArgumentException], new Date("not a date"))
-    assertThrows(classOf[IllegalArgumentException], Date.parse("not a date"))
-  }
+    // Explicit GMT time zone
+    test(878707407000L, "Nov 5 1997 5:23:27 GMT")
+    test(878342400000L, "Nov 1 1997 GMT")
+    test(65461000L, "Jan 1 1970 18:11:01 GMT")
+    test(878707407000L, "Nov 5 1997 (ig(no)ré) 5:23:27 GMT")
 
-  @Test def after(): Unit = {
-    assertFalse(new Date(97, 11, 5, 0, 0).after(new Date(98, 11, 5, 0, 0)))
-    assertTrue(new Date(99, 11, 5, 0, 0).after(new Date(98, 11, 5, 0, 0)))
-    assertFalse(new Date(99, 11, 5, 0, 0).after(new Date(99, 11, 5, 0, 0)))
-  }
+    // Implicit time zone -> local time zone
+    test(new Date(97, 10, 5, 5, 23, 27).getTime(), "Nov 5 1997 5:23:27")
+    test(new Date(97, 10, 1).getTime(), "Nov 1 1997")
+    test(new Date(70, 0, 1, 18, 11, 1).getTime(), "Jan 1 1970 18:11:01")
 
-  @Test def before(): Unit = {
-    assertTrue(new Date(97, 11, 5, 0, 0).before(new Date(98, 11, 5, 0, 0)))
-    assertFalse(new Date(99, 11, 5, 0, 0).before(new Date(98, 11, 5, 0, 0)))
-    assertFalse(new Date(99, 11, 5, 0, 0).before(new Date(99, 11, 5, 0, 0)))
+    testFailure("not a date")
   }
 
   @Test def cloneTest(): Unit = {
-    def testClone(date: Date): Boolean = {
+    def testClone(time: Long): Unit = {
+      val date = new Date(time)
       val cloned = date.clone()
-      date == cloned
+      assertNotSame(date, cloned)
+      assertEquals(date, cloned)
     }
 
-    assertTrue(testClone(new Date(97, 11, 5, 0, 0)))
-    assertTrue(testClone(new Date(92, 14, 6, 2, 1)))
-    assertTrue(testClone(new Date(4, 1, 2, 3, 0, 0)))
+    testClone(881276400000L)
+    testClone(731379660000L)
+    testClone(-2080072800000L)
   }
 
   @Test def getYear(): Unit = {
@@ -108,10 +130,10 @@ class DateTest {
   }
 
   @Test def toGMTString(): Unit = {
-    assertEquals("31 Dec 1899 00:00:00 GMT", new Date(Date.UTC(0, 0, 0, 0, 0, 0)).toGMTString)
-    assertEquals("3 Nov 1997 05:23:27 GMT", new Date(Date.UTC(97, 10, 3, 5, 23, 27)).toGMTString)
-    assertEquals("5 Jan 1902 08:01:09 GMT", new Date(Date.UTC(1, 12, 5, 8, 1, 9)).toGMTString)
-    assertEquals("9 Jan 2900 05:03:04 GMT", new Date(Date.UTC(1000, 0, 9, 5, 3, 4)).toGMTString)
+    assertEquals("3 Nov 1997 05:23:27 GMT", new Date(878534607567L).toGMTString())
+    assertEquals("31 Dec 1899 00:00:00 GMT", new Date(-2209075200000L).toGMTString())
+    assertEquals("5 Jan 1902 08:01:09 GMT", new Date(-2145542330500L).toGMTString())
+    assertEquals("9 Jan 2900 05:03:04 GMT", new Date(29348715784999L).toGMTString())
   }
 
   // #4131
