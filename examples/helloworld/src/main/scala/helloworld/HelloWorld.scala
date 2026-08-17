ab32b6dddada28507a7504d04d65d7204b5fb29b
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-08-17 10:22:22 +0200

    Make the helloworld run in minimal Wasm.

--- examples/helloworld/src/main/scala/helloworld/HelloWorld.scala
+++ examples/helloworld/src/main/scala/helloworld/HelloWorld.scala
@@ -11,4 +11,20 @@ object HelloWorld {
 
     println("Hello world!")
   }
+
+  @noinline
+  def println(x: Any): Unit = {
+    import java.nio.charset.StandardCharsets
+    import scala.scalajs.LinkingInfo
+
+    LinkingInfo.linkTimeIf(LinkingInfo.moduleKind == LinkingInfo.ModuleKind.MinimalWasmModule) {
+      doWriteLine(0, x.toString().getBytes(StandardCharsets.UTF_8))
+    } {
+      System.out.println(x)
+    }
+  }
+
+  @scala.scalajs.wasm.minimal.annotation.WasmImport("scalajs:core", "doWriteLine")
+  def doWriteLine(isErr: scala.Int, line: Array[scala.Byte]): Unit =
+    scala.scalajs.wasm.native
 }
