1c9fdde34ec2ac19ffb769108da4c82ce0cfb00a
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-08-18 11:28:58 +0200

    Don't alter the public sbt plugin.

--- sbt-plugin/src/main/scala/org/scalajs/sbtplugin/ScalaJSPluginInternal.scala
+++ sbt-plugin/src/main/scala/org/scalajs/sbtplugin/ScalaJSPluginInternal.scala
@@ -546,10 +546,15 @@ private[sbtplugin] object ScalaJSPluginInternal {
         val path = (linkerOutputDir / mainModule.moduleFileName).toPath
 
         mainModule.moduleKind match {
-          case ModuleKind.NoModule          => Input.Script(path)
-          case ModuleKind.ESModule          => Input.ESModule(path)
-          case ModuleKind.CommonJSModule    => Input.CommonJSModule(path)
-          case ModuleKind.MinimalWasmModule => MinimalWasmInput.MinimalWasmModule(path)
+          case ModuleKind.NoModule       => Input.Script(path)
+          case ModuleKind.ESModule       => Input.ESModule(path)
+          case ModuleKind.CommonJSModule => Input.CommonJSModule(path)
+
+          case ModuleKind.MinimalWasmModule =>
+            throw new MessageOnlyException(
+                s"Cannot compute a jsEnvInput for ${mainModule.moduleKind}. " +
+                "It is up to you to set up your own jsEnvInput and jsEnv " +
+                s"if you need to run/test a project with ${mainModule.moduleKind}.")
         }
       },
 
