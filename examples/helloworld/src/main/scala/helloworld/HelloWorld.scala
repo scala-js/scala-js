7ef38c1c408fe4d528289a3b4b5d1061a8662887
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-08-20 13:19:05 +0200

    Make doWriteLine UTF-16-based.
    That remains the native encoding of Scala, so it makes more sense
    than UTF-8.

--- examples/helloworld/src/main/scala/helloworld/HelloWorld.scala
+++ examples/helloworld/src/main/scala/helloworld/HelloWorld.scala
@@ -14,17 +14,24 @@ object HelloWorld {
 
   @noinline
   def println(x: Any): Unit = {
-    import java.nio.charset.StandardCharsets
     import scala.scalajs.LinkingInfo
 
     LinkingInfo.linkTimeIf(LinkingInfo.moduleKind == LinkingInfo.ModuleKind.MinimalWasmModule) {
-      doWriteLine(0, x.toString().getBytes(StandardCharsets.UTF_8))
+      val s = x.toString()
+      val len = s.length()
+      val codeUnits = new Array[Short](len)
+      var i = 0
+      while (i != len) {
+        codeUnits(i) = s.charAt(i).toShort
+        i += 1
+      }
+      doWriteLine(0, codeUnits)
     } {
       System.out.println(x)
     }
   }
 
   @scala.scalajs.wasm.minimal.annotation.WasmImport("scalajs:core", "doWriteLine")
-  def doWriteLine(isErr: scala.Int, line: Array[scala.Byte]): Unit =
+  def doWriteLine(isErr: scala.Int, line: Array[Short]): Unit =
     scala.scalajs.wasm.native
 }
