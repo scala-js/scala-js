1c81b89094f11fed7e84da66ef8c43fac83ba3e4
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-09-13 17:37:48 +0200

    Add the rest of the shared test suite.

--- project/Build.scala
+++ project/Build.scala
@@ -2556,17 +2556,15 @@ object Build {
         } else {
           originalSources
             .filter(f =>
-              contains(f, "/shared/src/test/require-scala2/org/scalajs/testsuite/compiler/") ||
-              contains(f, "/shared/src/test/scala/org/scalajs/testsuite/compiler/") ||
-              contains(f, "/shared/src/test/scala/org/scalajs/testsuite/javalib/") && (
+              contains(f, "/shared/src/test/") && (
                 !endsWith(f, "/ClassValueTest.scala") && // TODO implement without JS interop
                 !endsWith(f, "/URITest.scala") && // TODO implement without JS interop
-                // TODO implement ju.regex.*
-                !contains(f, "/shared/src/test/scala/org/scalajs/testsuite/javalib/util/regex/") &&
+                !endsWith(f, "/EnumerationTest.scala") && // TODO implement without String.split
+                !endsWith(f, "/SymbolTest.scala") && // TODO implement without JS interop
+                !endsWith(f, "/SymbolTestScala2.scala") && // TODO implement without JS interop
+                !contains(f, "/testsuite/javalib/util/regex/") && // TODO implement ju.regex.*
                 !endsWith(f, "/ThreadLocalRandomTest.scala") // no seed; needs initial random number
               ) ||
-              contains(f, "/shared/src/test/scala/org/scalajs/testsuite/niobuffer/") ||
-              contains(f, "/shared/src/test/scala/org/scalajs/testsuite/utils/") ||
               contains(f, "/js-wasm/src/test/")
             )
         }
